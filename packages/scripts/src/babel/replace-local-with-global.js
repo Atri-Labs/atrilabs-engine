d3ab23e8392454bdcec56f1f0d301b013902a2c0
cruxcode <swaroopshyam0@gmail.com>
2022-04-15 16:09:04 +0530

    babel plugin for replacing menu names etc.

---
+++ packages/scripts/src/babel/replace-local-with-global.js
@@ -0,0 +1,66 @@
+/**
+ *
+ * @param {*} babel
+ * @param {{getNameMap: (filename: string)=>{menu: {[localname: string]: string},containers: {[localname: string]: string},tabs: {[localname: string]: string}}}} options
+ * @returns
+ */
+module.exports = function (babel, options) {
+  let filename = "";
+  return {
+    visitor: {
+      Program(path, parent) {
+        filename = parent.filename;
+      },
+      CallExpression(path) {
+        const map = options.getNameMap(filename);
+        if (
+          path.node.callee.name === "menu" &&
+          path.get("arguments.0") &&
+          path.get("arguments.0").type === "StringLiteral"
+        ) {
+          const name = path.get("arguments.0").node.value;
+          if (map.menu) {
+            const menu = map.menu;
+            if (menu[name]) {
+              path
+                .get("arguments.0")
+                .replaceWithSourceString(`"${menu[name]}"`);
+            }
+          }
+        }
+
+        if (
+          path.node.callee.name === "container" &&
+          path.get("arguments.0") &&
+          path.get("arguments.0").type === "StringLiteral"
+        ) {
+          const name = path.get("arguments.0").node.value;
+          if (map.containers) {
+            const containers = map.containers;
+            if (containers[name]) {
+              path
+                .get("arguments.0")
+                .replaceWithSourceString(`"${containers[name]}"`);
+            }
+          }
+        }
+
+        if (
+          path.node.callee.name === "tab" &&
+          path.get("arguments.0") &&
+          path.get("arguments.0").type === "StringLiteral"
+        ) {
+          const name = path.get("arguments.0").node.value;
+          if (map.tabs) {
+            const tabs = map.tabs;
+            if (tabs[name]) {
+              path
+                .get("arguments.0")
+                .replaceWithSourceString(`"${tabs[name]}"`);
+            }
+          }
+        }
+      },
+    },
+  };
+};
