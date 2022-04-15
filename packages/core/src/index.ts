d0fcd9fecb122a74c43012c9ba4d4ca0bb796f88
cruxcode <swaroopshyam0@gmail.com>
2022-04-15 13:30:03 +0530

    api change in core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -5,14 +5,18 @@ import "./layers";
 
 export function setApp(_app: JsxElement) {}
 
-export const menu = {
+/**
+ *
+ * @param name Pass a local value. The local value must be a string literal as it's replaced with global value during build.
+ * @returns
+ */
+export function menu(name: string) {
   /**
    *
-   * @param name global name of the menu
    * @param item menu item
    * @returns
    */
-  register: (name: string, item: MenuItem<any>): void => {
+  const register = (item: MenuItem<any>): void => {
     if (menuRegistry[name] === undefined) {
       console.error(
         `Menu with name ${name} does not exist.\nMake sure that the menu is registered by a layer in layer.config.js\nOR\nMake sure to remap the layer in tool.confgi.js.`
@@ -20,17 +24,23 @@ export const menu = {
       return;
     }
     menuRegistry[name]!.items.push(item);
-  },
-};
+  };
 
-export const container = {
+  return { register };
+}
+
+/**
+ *
+ * @param name Pass a local value. The local value must be a string literal as it's replaced with global value during build.
+ * @returns
+ */
+export function container(name: string) {
   /**
    *
-   * @param name global name of the menu
    * @param item container
    * @returns
    */
-  register: (name: string, item: Container<any>): void => {
+  const register = (item: Container<any>): void => {
     if (containerRegistry[name] === undefined) {
       console.error(
         `Container with name ${name} does not exist.\nMake sure that the container is registered by a layer in layer.config.js\nOR\nMake sure to remap the layer in tool.confgi.js.`
@@ -38,17 +48,23 @@ export const container = {
       return;
     }
     containerRegistry[name]!.items.push(item);
-  },
-};
+  };
 
-export const tabs = {
+  return { register };
+}
+
+/**
+ *
+ * @param name Pass a local value. The local value must be a string literal as it's replaced with global value during build.
+ * @returns
+ */
+export function tab(name: string) {
   /**
    *
-   * @param name global name of the menu
    * @param item tab item
    * @returns
    */
-  register: (name: string, item: TabItem<any>): void => {
+  const register = (item: TabItem<any>): void => {
     if (tabsRegistry[name] === undefined) {
       console.error(
         `Tab bar with name ${name} does not exist.\nMake sure that the tab bar is registered by a layer in layer.config.js\nOR\nMake sure to remap the layer in tool.confgi.js.`
@@ -56,8 +72,10 @@ export const tabs = {
       return;
     }
     tabsRegistry[name]!.items.push(item);
-  },
-};
+  };
+
+  return { register };
+}
 
 // types
 
