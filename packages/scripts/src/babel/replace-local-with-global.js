884e80edea57b64eac9fdb0b88195fde2e200aca
cruxcode <swaroopshyam0@gmail.com>
2022-04-18 14:14:34 +0530

    code refactored

--- packages/scripts/src/babel/replace-local-with-global.js
+++ packages/scripts/src/babel/replace-local-with-global.js
@@ -1,3 +1,55 @@
+const callExprVisitor = {
+  CallExpression(path) {
+    const options = this.options;
+    const filename = this.filename;
+    const map = options.getNameMap(filename);
+
+    if (
+      path.node.callee.name === "menu" &&
+      path.get("arguments.0") &&
+      path.get("arguments.0").type === "StringLiteral"
+    ) {
+      const name = path.get("arguments.0").node.value;
+      if (map.menu) {
+        const menu = map.menu;
+        if (menu[name]) {
+          path.get("arguments.0").replaceWithSourceString(`"${menu[name]}"`);
+        }
+      }
+    }
+
+    if (
+      path.node.callee.name === "container" &&
+      path.get("arguments.0") &&
+      path.get("arguments.0").type === "StringLiteral"
+    ) {
+      const name = path.get("arguments.0").node.value;
+      if (map.containers) {
+        const containers = map.containers;
+        if (containers[name]) {
+          path
+            .get("arguments.0")
+            .replaceWithSourceString(`"${containers[name]}"`);
+        }
+      }
+    }
+
+    if (
+      path.node.callee.name === "tab" &&
+      path.get("arguments.0") &&
+      path.get("arguments.0").type === "StringLiteral"
+    ) {
+      const name = path.get("arguments.0").node.value;
+      if (map.tabs) {
+        const tabs = map.tabs;
+        if (tabs[name]) {
+          path.get("arguments.0").replaceWithSourceString(`"${tabs[name]}"`);
+        }
+      }
+    }
+  },
+};
+
 /**
  *
  * @param {*} babel
@@ -10,56 +62,7 @@ module.exports = function (babel, options) {
     visitor: {
       Program(path, parent) {
         filename = parent.filename;
-      },
-      CallExpression(path) {
-        const map = options.getNameMap(filename);
-        if (
-          path.node.callee.name === "menu" &&
-          path.get("arguments.0") &&
-          path.get("arguments.0").type === "StringLiteral"
-        ) {
-          const name = path.get("arguments.0").node.value;
-          if (map.menu) {
-            const menu = map.menu;
-            if (menu[name]) {
-              path
-                .get("arguments.0")
-                .replaceWithSourceString(`"${menu[name]}"`);
-            }
-          }
-        }
-
-        if (
-          path.node.callee.name === "container" &&
-          path.get("arguments.0") &&
-          path.get("arguments.0").type === "StringLiteral"
-        ) {
-          const name = path.get("arguments.0").node.value;
-          if (map.containers) {
-            const containers = map.containers;
-            if (containers[name]) {
-              path
-                .get("arguments.0")
-                .replaceWithSourceString(`"${containers[name]}"`);
-            }
-          }
-        }
-
-        if (
-          path.node.callee.name === "tab" &&
-          path.get("arguments.0") &&
-          path.get("arguments.0").type === "StringLiteral"
-        ) {
-          const name = path.get("arguments.0").node.value;
-          if (map.tabs) {
-            const tabs = map.tabs;
-            if (tabs[name]) {
-              path
-                .get("arguments.0")
-                .replaceWithSourceString(`"${tabs[name]}"`);
-            }
-          }
-        }
+        path.traverse(callExprVisitor, { options, filename });
       },
     },
   };
