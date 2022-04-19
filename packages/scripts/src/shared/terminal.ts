4f2528b0e432176936e2f3e03c85c8fb97f9bfe1
cruxcode <swaroopshyam0@gmail.com>
2022-04-19 18:18:43 +0530

    wip: webpack dev server doesn't captures terminal

---
+++ packages/scripts/src/shared/terminal.ts
@@ -0,0 +1,7 @@
+export const isInteractive = process.stdout.isTTY;
+
+export const clearConsole = () => {
+  process.stdout.write(
+    process.platform === "win32" ? "\x1B[2J\x1B[0f" : "\x1B[2J\x1B[3J\x1B[H"
+  );
+};
--- packages/scripts/src/tasks/dev/dev.ts
+++ packages/scripts/src/tasks/dev/dev.ts
@@ -1,1 +1,59 @@
 #!/usr/bin/env node
+import { webpack } from "webpack";
+import WebpackDevServer from "webpack-dev-server";
+import chalk from "chalk";
+import { createGlobalModuleForLayer } from "../../shared/processLayer";
+import {
+  extractLayerEntries,
+  getToolPkgInfo,
+  importToolConfig,
+} from "../../shared/utils";
+import createWebpackConfig from "../../shared/webpack.config";
+import { clearConsole, isInteractive } from "../../shared/terminal";
+console.log("dev called");
+const toolPkgInfo = getToolPkgInfo();
+
+importToolConfig(toolPkgInfo.configFile)
+  .then(async (toolConfig) => {
+    const layerEntries = await extractLayerEntries(toolConfig, toolPkgInfo);
+
+    // create global module and symlink for each layer
+    layerEntries.forEach((layerEntry) => {
+      createGlobalModuleForLayer(layerEntry);
+    });
+
+    // create webpack config and compiler
+    const webpackConfig = createWebpackConfig(
+      toolPkgInfo,
+      toolConfig,
+      layerEntries
+    );
+    webpackConfig["devServer"] = {
+      watchFiles: { paths: [toolPkgInfo.configFile] },
+    };
+    const compiler = webpack(webpackConfig);
+
+    // create dev server
+    const devServer = new WebpackDevServer(
+      { host: "localhost", port: 4000 },
+      compiler
+    );
+
+    // launch WebpackDevServer
+    devServer.startCallback(() => {
+      if (isInteractive) {
+        clearConsole();
+      }
+      console.log(chalk.cyan("Starting the development server...\n"));
+    });
+
+    ["SIGINT", "SIGTERM"].forEach(function (sig) {
+      process.on(sig, function () {
+        devServer.close();
+        process.exit();
+      });
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
