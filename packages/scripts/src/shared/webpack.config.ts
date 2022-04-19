6921ba26a4ba9703431b577bc39428b66c2b8ba7
cruxcode <swaroopshyam0@gmail.com>
2022-04-19 17:50:18 +0530

    webpack config moved in a seperate file

---
+++ packages/scripts/src/shared/webpack.config.ts
@@ -0,0 +1,52 @@
+import { ToolConfig } from "@atrilabs/core";
+import path from "path";
+import { Configuration } from "webpack";
+import { LayerEntry, ToolPkgInfo } from "./types";
+import emitBabelLoader from "./emitBabelLoader";
+
+export default function createWebpackConfig(
+  toolPkgInfo: ToolPkgInfo,
+  toolConfig: ToolConfig,
+  layerEntries: LayerEntry[]
+) {
+  const webpackConfig: Configuration = {
+    target: "web",
+    entry: {
+      layers: {
+        import: require.resolve("@atrilabs/core/lib/layers.js"),
+        dependOn: "shared",
+      },
+      shared: ["react", "react-dom"],
+    },
+    /**
+     * Inlcude source map in the bundle for devtools.
+     */
+    devtool: "source-map",
+    output: {
+      path: path.resolve(toolPkgInfo.dir, toolConfig.output),
+    },
+    module: {
+      rules: [
+        /**
+         * Loads source maps for packages in node_modules.
+         * Layers will generally be located in node_modules.
+         */
+        {
+          enforce: "pre",
+          exclude: /@babel(?:\/|\\{1,2})runtime/,
+          test: /\.(js|mjs|jsx|ts|tsx|css)$/,
+          loader: require.resolve("source-map-loader"),
+        },
+        {
+          oneOf: [
+            {
+              test: /\.(js|mjs|jsx|ts|tsx)$/,
+              use: emitBabelLoader(layerEntries),
+            },
+          ],
+        },
+      ],
+    },
+  };
+  return webpackConfig;
+}
--- packages/scripts/src/tasks/build/build.ts
+++ packages/scripts/src/tasks/build/build.ts
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
-import { webpack, Configuration } from "webpack";
-import path from "path";
+console.log("lern dev");
+import { webpack } from "webpack";
 import {
   extractLayerEntries,
   getToolPkgInfo,
   importToolConfig,
 } from "../../shared/utils";
-import emitBabelLoader from "../../shared/emitBabelLoader";
 import { createGlobalModuleForLayer } from "../../shared/processLayer";
+import createWebpackConfig from "../../shared/webpack.config";
 
 const toolPkgInfo = getToolPkgInfo();
 
@@ -21,45 +21,11 @@ importToolConfig(toolPkgInfo.configFile)
     });
 
     // bundle ui
-    const webpackConfig: Configuration = {
-      target: "web",
-      entry: {
-        layers: {
-          import: require.resolve("@atrilabs/core/lib/layers.js"),
-          dependOn: "shared",
-        },
-        shared: ["react", "react-dom"],
-      },
-      /**
-       * Inlcude source map in the bundle for devtools.
-       */
-      devtool: "source-map",
-      output: {
-        path: path.resolve(toolPkgInfo.dir, toolConfig.output),
-      },
-      module: {
-        rules: [
-          /**
-           * Loads source maps for packages in node_modules.
-           * Layers will generally be located in node_modules.
-           */
-          {
-            enforce: "pre",
-            exclude: /@babel(?:\/|\\{1,2})runtime/,
-            test: /\.(js|mjs|jsx|ts|tsx|css)$/,
-            loader: require.resolve("source-map-loader"),
-          },
-          {
-            oneOf: [
-              {
-                test: /\.(js|mjs|jsx|ts|tsx)$/,
-                use: emitBabelLoader(layerEntries),
-              },
-            ],
-          },
-        ],
-      },
-    };
+    const webpackConfig = createWebpackConfig(
+      toolPkgInfo,
+      toolConfig,
+      layerEntries
+    );
     webpack(webpackConfig, (err, stats) => {
       let buildFailed = false;
       if (err) {
