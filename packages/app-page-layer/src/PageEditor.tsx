2b64494c83fb9b5e0402a41e2869e759737339eb
cruxcode <swaroopshyam0@gmail.com>
2022-05-05 21:23:45 +0530

    CreatePage moved to a different file

--- packages/app-page-layer/src/PageEditor.tsx
+++ packages/app-page-layer/src/PageEditor.tsx
@@ -1,23 +1,19 @@
 import {
   agastyaLine,
-  amber300,
   gray200,
   gray300,
   gray500,
   gray700,
-  gray800,
   gray900,
   h1Heading,
   h4Heading,
   smallText,
-  Input,
-  Dropdown,
 } from "@atrilabs/design-system";
 import { useState } from "react";
+import { CreatePage } from "./components/CreatePage";
 import { Cross } from "./icons/Cross";
 import { DownArrow } from "./icons/DownArrow";
 import { Folder } from "./icons/Folder";
-import { LinkIcon } from "./icons/LinkIcon";
 import { Maginfier } from "./icons/Magnifier";
 import { PageIcon } from "./icons/PageIcon";
 import { Setting } from "./icons/Setting";
@@ -103,43 +99,6 @@ const styles: { [key: string]: React.CSSProperties } = {
     alignItems: "center",
     justifyContent: "space-between",
     borderBottom: `1px solid ${agastyaLine}`,
-  },
-  // ================create page dialog box=====
-  createPage: {
-    position: "absolute",
-    left: "100%",
-    top: 0,
-    width: "15rem",
-    background: gray700,
-    borderLeft: `1px solid ${gray800}`,
-    display: "flex",
-    flexDirection: "column",
-  },
-  createPageHeader: {
-    display: "flex",
-    padding: `0.5rem 1rem`,
-    justifyContent: "space-between",
-    borderBottom: `1px solid ${gray800}`,
-    paddingBottom: "0.5rem",
-  },
-  createPageFormField: {
-    display: "flex",
-    justifyContent: "space-between",
-    ...smallText,
-    color: gray300,
-    alignItems: "center",
-    padding: `1rem 1rem 0 1rem`,
-  },
-  slugContainer: {
-    padding: `1rem 1rem 0 1rem`,
-  },
-  slugContent: {
-    display: "flex",
-    background: gray500,
-    alignItems: "center",
-    ...smallText,
-    color: gray300,
-    borderRadius: "2px",
   },
   // ================create folder dialog box===
   createFolder: {
@@ -238,54 +197,7 @@ export const PageEditor = () => {
         </main>
       </section>
 
-      <div style={styles.createPage}>
-        <div style={styles.createPageHeader}>
-          <h4 style={styles.pageContHeaderH4}>Create new page</h4>
-          <span style={styles.iconsSpan}>
-            <Cross />
-          </span>
-        </div>
-        <div style={styles.createPageFormField}>
-          <span>Folder</span>
-          <Dropdown options={["Folder 1", "Folder 2"]} />
-        </div>
-        <div style={styles.createPageFormField}>
-          <span>Page</span>
-          <Input />
-        </div>
-        <div style={styles.slugContainer}>
-          <div style={styles.slugContent}>
-            <div
-              style={{
-                padding: "0.5rem",
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              <LinkIcon />
-            </div>
-            <div>/folder1/page1</div>
-          </div>
-        </div>
-        <div
-          style={{ display: "flex", justifyContent: "center", padding: "1rem" }}
-        >
-          <button
-            style={{
-              borderRadius: "4px",
-              backgroundColor: amber300,
-              ...h4Heading,
-              color: gray900,
-              border: "none",
-              padding: "0.2rem 0.6rem 0.2rem 0.6rem",
-            }}
-          >
-            Create
-          </button>
-        </div>
-      </div>
-
-      {/* <div style={styles.createFolder}></div> */}
+      <CreatePage />
     </div>
   );
 };
