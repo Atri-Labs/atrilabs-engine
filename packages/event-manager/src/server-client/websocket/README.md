699a8cdefaf05db8cbf36f3a676d92d6f9a77b44
cruxcode <swaroopshyam0@gmail.com>
2022-05-07 21:34:54 +0530

    wip: create page/folder backend

--- packages/event-manager/src/server-client/websocket/README.md
+++ packages/event-manager/src/server-client/websocket/README.md
@@ -1,3 +1,7 @@
 # `websocket`
 
 This server-client uses event manager from `implementation/lowdb`. This server-client also arranges pages inside folders. It's able to achieve this by storing the hierarchy of folders/pages using the Meta File API exposed by `implementation/lowdb`.
+
+# Schema for meta.json
+
+`{ folders: { folderId: { name: "", id: "", parentId: "" } }, pages: {pageId: "folderId"} }`
--- packages/event-manager/src/server-client/websocket/server.ts
+++ packages/event-manager/src/server-client/websocket/server.ts
@@ -21,16 +21,43 @@ const forests = readForests();
 // create one directory and event manager for each of forest
 const eventManagers = createEventManager(forests);
 
+function getMeta(forestname: string) {
+  const meta = eventManagers[forestname]!.meta();
+  if (meta["folders"] === undefined) {
+    meta["folders"] = { root: { id: "root", name: "/", parentId: "" } };
+  }
+  if (meta["pages"] === undefined) {
+    meta["pages"] = {};
+  }
+  return meta;
+}
+
 io.on("connection", (socket) => {
   socket.on("createFolder", (forestname, folder, callback) => {
     console.log(folder);
     if (eventManagers[forestname]) {
-      const meta = eventManagers[forestname]!.meta();
-      meta[folder.id] = folder;
+      const meta = getMeta(forestname);
+      meta["folders"][folder.id] = folder;
       eventManagers[forestname]!.updateMeta(meta);
       callback(true);
     } else {
       callback(false);
+    }
+  });
+  socket.on("createPage", (forestname, page, callback) => {
+    if (eventManagers[forestname]) {
+      const meta = getMeta(forestname);
+      // folder should exist already
+      if (meta["folders"][page.folderId]) {
+        const foldername = meta["folders"][page.folderId].name;
+        // TODO: route must follow the hierarchy of folders, not just the immidiate folder
+        const route = `/${foldername}/${page.name}`;
+        meta["pages"][page.id] = page.folderId;
+        eventManagers[forestname]!.createPage(page.id, page.name, route);
+        callback(true);
+      } else {
+        callback(false);
+      }
     }
   });
 });
