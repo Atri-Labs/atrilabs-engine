e569c29f0f26588139d91524d0ba44bfee5e76d3
cruxcode <swaroopshyam0@gmail.com>
2022-05-08 00:48:03 +0530

    tool.config.json has forestManger & services

--- packages/webapp-builder/src/tool.config.js
+++ packages/webapp-builder/src/tool.config.js
@@ -1,17 +1,28 @@
+const forestsConfig = {
+  page: [
+    {
+      pkg: "@atrilabs/basic-trees",
+      modulePath: "lib/componentTree.js",
+      name: "componentTree",
+    },
+    {
+      pkg: "@atrilabs/basic-trees",
+      modulePath: "lib/cssTree.js",
+      name: "cssTree",
+    },
+  ],
+};
+
 module.exports = {
-  forests: {
-    page: [
-      {
-        pkg: "@atrilabs/basic-trees",
-        modulePath: "lib/componentTree.js",
-        name: "componentTree",
-      },
-      {
-        pkg: "@atrilabs/basic-trees",
-        modulePath: "lib/cssTree.js",
-        name: "cssTree",
-      },
-    ],
+  forests: forestsConfig,
+  forestManager: {
+    path: require.resolve(
+      "@atrilabs/forest/lib/implementations/lowdb/LowDbForestManager"
+    ),
+    options: {
+      forestDir: "",
+      forestsConfig: forestsConfig,
+    },
   },
   layers: [
     { pkg: "@atrilabs/base-layer" },
@@ -20,4 +31,10 @@ module.exports = {
     { pkg: "@atrilabs/app-page-layer" },
   ],
   output: "lib",
+  services: {
+    eventServer: {
+      path: require.resolve("@atrilabs/server-client/lib/websocket/server"),
+    },
+    codeGenerators: [],
+  },
 };
