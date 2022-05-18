dd0aca5998d98ce2fb2343fd26dc2c5df1167970
cruxcode <swaroopshyam0@gmail.com>
2022-05-19 02:04:26 +0530

    added manifest registry methods

---
+++ packages/core/src/manifestRegistry.ts
@@ -0,0 +1,33 @@
+import { ManifestSchema } from "./types";
+
+// The object will be filled during build time automatically
+// schema will be imported and mapped
+// components will be left as empty array to be filled during runtime
+const manifestRegistry: {
+  [manifestId: string]: { schema: ManifestSchema; components: any[] };
+} = {};
+
+const manifestRegistrySubscribers: (() => void)[] = [];
+
+function subscribe(cb: () => void) {
+  manifestRegistrySubscribers.push(cb);
+}
+
+function readManifestRegistry() {
+  const manifestIds = Object.keys(manifestRegistry);
+  const copy = { ...manifestRegistry };
+  manifestIds.forEach((id) => {
+    copy[id] = {
+      schema: { ...manifestRegistry[id].schema },
+      components: [...manifestRegistry[id].components],
+    };
+  });
+  return copy;
+}
+
+function writeComponents(manifestId: string, components: []) {
+  manifestRegistry[manifestId].components = components;
+  manifestRegistrySubscribers.forEach((cb) => cb());
+}
+
+export default { readManifestRegistry, writeComponents, subscribe };
