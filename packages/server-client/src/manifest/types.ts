0bf23ae34703806590ebc027b2115ad60d088338
cruxcode <swaroopshyam0@gmail.com>
2022-05-19 12:25:32 +0530

    wip: server-client types

--- packages/server-client/src/manifest/types.ts
+++ packages/server-client/src/manifest/types.ts
@@ -1,1 +1,23 @@
+export type Script = {
+  src: string;
+};
 
+export type ManifestPkg = {
+  pkg: string;
+};
+
+export interface ServerToClientEvents {
+  updateManifestPkg: (
+    manifest: ManifestPkg,
+    script: Script,
+    callback: (success: boolean) => void
+  ) => void;
+}
+
+export interface ClientToServerEvents {
+  sendManifestScripts: (callback: (script: Script) => void) => void;
+}
+
+export interface InterServerEvents {}
+
+export interface SocketData {}
