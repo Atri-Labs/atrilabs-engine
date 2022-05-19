ece9b13b1ff87fcee50fba1dafb071604fab75a8
cruxcode <swaroopshyam0@gmail.com>
2022-05-19 12:06:03 +0530

    manifest shim for react created

---
+++ packages/manifest-shims/src/react/manifests.ts
@@ -0,0 +1,6 @@
+/**
+ * Default exports from each file goes in here.
+ * The schema validation is expected to be carried out client side.
+ * The array is expected to be filled by some automated code before build.
+ */
+export const manifests = [];
