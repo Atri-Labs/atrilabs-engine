5edeac613efa643bff1ef962ab7bea879457daff
cruxcode <swaroopshyam0@gmail.com>
2022-05-21 08:52:30 +0530

    manifest bundles are delivered correctly

--- packages/server-client/src/manifest/server.ts
+++ packages/server-client/src/manifest/server.ts
@@ -1,5 +1,8 @@
 import fs from "fs";
 import path from "path";
+import express from "express";
+import http from "http";
+import cors from "cors";
 import { Server } from "socket.io";
 import { ToolConfig } from "@atrilabs/core";
 import {
@@ -9,6 +12,7 @@ import {
   compileTypescriptManifestPkg,
   bundleManifestPkg,
   copyManifestEntryTemplate,
+  ManifestPkgInfo,
 } from "@atrilabs/scripts";
 import {
   ClientToServerEvents,
@@ -22,18 +26,63 @@ export type ManifestServerOptions = {
   port?: number;
 };
 
+const app = express();
+const server = http.createServer(app);
+
+// set cors for http server
+app.use(cors({ origin: "*" }));
+
+function getAllPaths(manifestPkgInfo: ManifestPkgInfo) {
+  const cacheDir = getManifestPkgCacheDir(manifestPkgInfo);
+  const cacheSrcDir = path.resolve(cacheDir, "src");
+  const firstBuild = path.resolve(cacheDir, "first-build");
+  const finalBuild = path.resolve(cacheDir, "final-build");
+  const entryPoint = path.resolve(cacheSrcDir, "index.js");
+  return { cacheDir, cacheSrcDir, firstBuild, finalBuild, entryPoint };
+}
+
 export default function (
   toolConfig: ToolConfig,
   options: ManifestServerOptions
 ) {
+  app.use((req, _res, next) => {
+    console.log("req rec", req.originalUrl);
+    next();
+  });
+  // setup http request & response (a file server for bundled manifest assets)
+  app.get("/assets", (req, res) => {
+    const pkg = req.query["pkg"] as string;
+    if (!pkg) {
+      res.status(400).send("Manifest package name is missing in the url.");
+      return;
+    }
+    const assetURI = req.query["file"] as string;
+    if (!assetURI) {
+      res.status(400).send("The url does not contain asset URI.");
+      return;
+    }
+
+    const decodedPkg = decodeURI(pkg);
+    const manifestPkgInfo = getManifestPkgInfo(decodedPkg);
+    const { finalBuild } = getAllPaths(manifestPkgInfo);
+    const assetPath = path.resolve(finalBuild, assetURI);
+    if (fs.existsSync(assetPath)) {
+      res.sendFile(assetPath);
+    } else {
+      console.log("Asset Not Found:", assetPath);
+      res.status(404).send("Request asset not found.");
+    }
+  });
+
   const manifestDirs = toolConfig.manifestDirs;
   const io = new Server<
     ClientToServerEvents,
     ServerToClientEvents,
     InterServerEvents,
     SocketData
-  >({ cors: { origin: "*" } });
+  >(server, { cors: { origin: "*" } }); // set cors for ws server
 
+  // setup socket request & response
   io.on("connection", (socket) => {
     socket.on("sendManifestScripts", async (cb) => {
       const manifestPkgBundles: ManifestPkgBundle[] = [];
@@ -44,11 +93,8 @@ export default function (
         const pkg = dir.pkg;
         const manifestPkgInfo = getManifestPkgInfo(pkg);
         // create cache directory if not already created
-        const cacheDir = getManifestPkgCacheDir(manifestPkgInfo);
-        const cacheSrcDir = path.resolve(cacheDir, "src");
-        const firstBuild = path.resolve(cacheDir, "first-build");
-        const finalBuild = path.resolve(cacheDir, "final-build");
-        const entryPoint = path.resolve(cacheSrcDir, "index.js");
+        const { cacheDir, cacheSrcDir, firstBuild, finalBuild, entryPoint } =
+          getAllPaths(manifestPkgInfo);
         if (!fs.existsSync(cacheDir)) {
           fs.mkdirSync(cacheDir, { recursive: true });
         }
@@ -68,7 +114,7 @@ export default function (
           entryPoint,
           { path: finalBuild, filename: "bundle.js" },
           scriptName,
-          `http://localhost:${port}/assets/${encodeURI(pkg)}`
+          `http://localhost:${port}/assets?pkg=${encodeURI(pkg)}&file=`
         );
         manifestPkgBundles.push({
           src: fs
@@ -83,5 +129,7 @@ export default function (
   });
 
   const port = (options && options.port) || 4003;
-  io.listen(port);
+  server.listen(port, () => {
+    console.log(`[manifest_server] address ${server.address()}`);
+  });
 }
