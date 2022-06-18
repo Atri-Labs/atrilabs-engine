120a3700977e29ffec0722f3d6c7eae2c1d342e9
cruxcode <swaroopshyam0@gmail.com>
2022-06-18 14:34:42 +0530

    UI for Publish added

--- packages/publish-app-layer/src/index.tsx
+++ packages/publish-app-layer/src/index.tsx
@@ -1,3 +1,44 @@
+import { Menu } from "@atrilabs/core";
+import { gray300, smallText } from "@atrilabs/design-system";
+import React, { useState } from "react";
+
+const styles: { [key: string]: React.CSSProperties } = {
+  outerDiv: {
+    ...smallText,
+    color: gray300,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+    position: "relative",
+  },
+  popupDiv: {
+    display: "flex",
+    flexDirection: "column",
+    position: "absolute",
+    top: "100%",
+    backgroundColor: "black",
+    width: "10rem",
+    zIndex: 1,
+    right: 0,
+  },
+};
+
 export default function () {
-  return <></>;
+  const [showPopup, setShowPopup] = useState(false);
+  return (
+    <>
+      <Menu name="PublishMenu">
+        <div
+          style={styles.outerDiv}
+          onClick={() => {
+            setShowPopup(!showPopup);
+          }}
+        >
+          Publish
+          {showPopup ? <div style={styles.popupDiv}>Some Status</div> : null}
+        </div>
+      </Menu>
+    </>
+  );
 }
