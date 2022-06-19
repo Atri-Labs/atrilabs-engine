43586c23f05eed50b23809df765aaee82190b4f5
cruxcode <swaroopshyam0@gmail.com>
2022-06-19 09:31:24 +0530

    generateApp & buildApp is called

--- packages/server-client/src/publish-app/server-utils.ts
+++ packages/server-client/src/publish-app/server-utils.ts
@@ -1,3 +1,5 @@
+import { exec } from "child_process";
+
 export const GENERATE = "generate";
 export const BUILD = "build";
 export const DEPLOY = "deploy";
@@ -20,11 +22,74 @@ export function createTaskQueue(startTask: string, endTask: string) {
   return taskQueue;
 }
 
+// resolve if task success else reject
+type FnQueue = (() => Promise<void>)[];
+
+async function generateApp() {
+  return new Promise<void>((res, rej) => {
+    exec("yarn run generateApp", (err, _stdout, stderr) => {
+      if (err) {
+        rej();
+        return;
+      }
+      if (stderr) {
+        rej();
+        return;
+      }
+      res();
+    });
+  });
+}
+
+async function buildApp() {
+  return new Promise<void>((res, rej) => {
+    exec("yarn run buildApp", (err, _stdout, stderr) => {
+      if (err) {
+        rej();
+        return;
+      }
+      if (stderr) {
+        rej();
+        return;
+      }
+      res();
+    });
+  });
+}
+
+async function deployApp() {}
+
 export function runTaskQueue(
   taskQueue: string[],
   onUpdate: (task: string, status: "success" | "failed") => void
 ) {
+  // create function queue
+  const fnQueue: FnQueue = [];
   taskQueue.forEach((task) => {
-    onUpdate(task, "success");
+    if (task === GENERATE) {
+      fnQueue.push(generateApp);
+    }
+    if (task === BUILD) {
+      fnQueue.push(buildApp);
+    }
+    if (task === DEPLOY) {
+      fnQueue.push(deployApp);
+    }
   });
+
+  // run functions one by one
+  function runFn(fnQueue: FnQueue, currFnIndex: number) {
+    if (currFnIndex < fnQueue.length) {
+      fnQueue[currFnIndex]!()
+        .then(() => {
+          onUpdate(taskQueue[currFnIndex]!, "success");
+          runFn(fnQueue, currFnIndex + 1);
+        })
+        .catch(() => {
+          onUpdate(taskQueue[currFnIndex]!, "failed");
+        });
+    }
+  }
+  let currFnIndex = 0;
+  runFn(fnQueue, currFnIndex);
 }
