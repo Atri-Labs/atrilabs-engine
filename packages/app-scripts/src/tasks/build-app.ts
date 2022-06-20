b9ce8cb5a3455ff06d2cb2465d26f4a821e0d822
cruxcode <swaroopshyam0@gmail.com>
2022-06-20 15:44:04 +0530

    build-app & build-server read atri-build-info.json

--- packages/app-scripts/src/tasks/build-app.ts
+++ packages/app-scripts/src/tasks/build-app.ts
@@ -1,13 +1,50 @@
 #!/usr/bin/node
-import path from "path";
-import { buildApp, getMode, setNodeAndBabelEnv } from "../shared/utils";
+import fs from "fs";
+import {
+  buildApp,
+  getMode,
+  setNodeAndBabelEnv,
+  buildInfoFile,
+  buildInfoFilename,
+} from "../shared/utils";
 
 const mode = getMode();
 setNodeAndBabelEnv(mode);
 
-const appEntry = path.resolve("app/src/index.jsx");
-const appHtml = path.resolve("app/public/index.html");
-const appOutput = path.resolve("dist/app");
-const includes = ["app/src"].map((str) => path.resolve(str));
-
-buildApp({ appEntry, appHtml, appOutput, includes, mode });
+try {
+  const buildInfo = JSON.parse(fs.readFileSync(buildInfoFile).toString());
+  if (buildInfo) {
+    const appEntry: string = buildInfo["appEntry"];
+    const appHtml: string = buildInfo["appHtml"];
+    const appOutput: string = buildInfo["appOutput"];
+    const appSrc: string = buildInfo["appSrc"];
+    const manifestDirs: { pkg: string }[] = buildInfo["manifestDirs"];
+    if (
+      appEntry &&
+      typeof appEntry === "string" &&
+      appHtml &&
+      typeof appHtml === "string" &&
+      appOutput &&
+      typeof appOutput === "string" &&
+      appSrc &&
+      typeof appSrc === "string" &&
+      manifestDirs &&
+      Array.isArray(manifestDirs)
+    ) {
+      throw Error(`Wrong schema of ${buildInfoFilename}.`);
+    }
+    const manifestPkgs = manifestDirs.map((manifestDef) => {
+      if (manifestDef["pkg"]) {
+        return manifestDef.pkg;
+      } else {
+        throw Error(`Wrong schema of ${buildInfoFilename}.`);
+      }
+    });
+    const includes = [...manifestPkgs, appSrc];
+    buildApp({ appEntry, appHtml, appOutput, includes, mode });
+  } else {
+    console.log(`Missing manifestDirs in ${buildInfoFilename}`);
+  }
+} catch (err) {
+  console.log(`Error\n`, err);
+}
--- packages/app-scripts/src/tasks/build-server.ts
+++ packages/app-scripts/src/tasks/build-server.ts
@@ -1,12 +1,39 @@
 #!/usr/bin/node
-import path from "path";
-import { buildServer, getMode, setNodeAndBabelEnv } from "../shared/utils";
+import fs from "fs";
+import {
+  buildInfoFile,
+  buildInfoFilename,
+  buildServer,
+  getMode,
+  setNodeAndBabelEnv,
+} from "../shared/utils";
 
 const mode = getMode();
 setNodeAndBabelEnv(mode);
 
-const serverEntry = path.resolve("server/src/index.tsx");
-const serverOutput = path.resolve("dist/server");
-const includes = ["server/src", "app/src"].map((str) => path.resolve(str));
-
-buildServer({ serverEntry, serverOutput, includes, mode });
+// Where will I get manifest directories to include?
+try {
+  const buildInfo = JSON.parse(fs.readFileSync(buildInfoFile).toString());
+  if (buildInfo) {
+    const serverEntry: string = buildInfo["serverEntry"];
+    const serverSrc: string = buildInfo["serverSrc"];
+    const serverOutput: string = buildInfo["serverOutput"];
+    const appSrc: string = buildInfo["appSrc"];
+    if (
+      serverEntry &&
+      typeof serverEntry === "string" &&
+      serverOutput &&
+      typeof serverOutput === "string" &&
+      serverSrc &&
+      typeof serverSrc === "string"
+    ) {
+      throw Error(`Wrong schema of ${buildInfoFilename}.`);
+    }
+    const includes = [serverSrc, appSrc];
+    buildServer({ serverEntry, serverOutput, includes, mode });
+  } else {
+    console.log(`Missing manifestDirs in ${buildInfoFilename}`);
+  }
+} catch (err) {
+  console.log(`Error\n`, err);
+}
