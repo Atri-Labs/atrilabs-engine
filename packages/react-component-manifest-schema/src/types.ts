a75aa4475f93995f0628fe1d5f2b985fa79d5e6d
cruxcode <swaroopshyam0@gmail.com>
2022-06-28 18:06:22 +0530

    wip: accomodate callbacks

--- packages/react-component-manifest-schema/src/types.ts
+++ packages/react-component-manifest-schema/src/types.ts
@@ -11,6 +11,23 @@ export type ComponentCoordsWM = {
   bottomWM: number;
 };
 
+/**
+ * controlled - This callback type will update the props on client side.
+ *              The signature is (oldProps, changeEvent) => (newProps).
+ * event      - This callback type will send an event to the backend.
+ *              The signature is (eventname, eventdata) => (void).
+ * upload     - This callback type will update the form data on client side.
+ *              This is primarily used to upload files.
+ *              The signature is (file handler | file handlers) => (void).
+ *
+ * // By default components won't have action handlers. The component creator
+ * can define some default action for a component.
+ * defaultActionHandler: {
+ *  [action]: [{sendFile: props.file}, {sendData: props.data}]
+ * }
+ */
+export type CallbackType = "controlled" | "event" | "upload";
+
 export type AcceptsChildFunction = (info: {
   coords: ComponentCoordsWM;
   childCoordinates: ComponentCoordsWM[];
@@ -34,7 +51,7 @@ export type ReactComponentManifestSchema = {
         canvasOptions: { groupByBreakpoint: boolean };
       };
     };
-    attachCallbacks: { [key: string]: any };
+    attachCallbacks: { [key: string]: { types: CallbackType[] } };
     acceptsChild?: AcceptsChildFunction;
   };
 };
