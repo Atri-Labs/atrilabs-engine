65a0608774ed9df2c9443d203bae5975ee04d23e
cruxcode <swaroopshyam0@gmail.com>
2022-07-13 03:03:17 +0530

    connect local can run in no-debug mode

--- python-packages/atri/src/atri/commands/connect_local.py
+++ python-packages/atri/src/atri/commands/connect_local.py
@@ -12,15 +12,17 @@ from ..utils.is_pkg_installed import is_pipenv_installed
 from ..utils.install_package import install_with_pipenv
 from shutil import copy
 import toml
+from ..utils.printd import printd
+from ..utils.globals import globals
 
 async def on_connect(sio):
     def print_success_cb(success):
         if success:
-            print("Registered as atri-cli with ipc server.")
+            printd("Registered as atri-cli with ipc server.")
     await sio.emit("registerAs", "atri-cli", callback=print_success_cb)
 
 async def connect_ipc_server(port: str):
-    print("Connecting to ipc server...")
+    printd("Connecting to ipc server...")
     sio = socketio.AsyncClient(
         reconnection=True,
         reconnection_attempts=inf,
@@ -31,16 +33,16 @@ async def connect_ipc_server(port: str):
 
     @sio.on('connect')
     async def connect_handler():
-        print('Connected to ipc server!')
+        printd('Connected to ipc server!')
         await on_connect(sio)
 
     @sio.event
     def connect_error(data):
-        print("Connection to ipc server failed!")
+        printd("Connection to ipc server failed!")
 
     @sio.event
     def disconnect():
-        print("Disconnected from ipc server!")
+        printd("Disconnected from ipc server!")
 
     while True:
         try:
@@ -54,7 +56,7 @@ def handle_ipc_events(sio, paths):
     python_server_proc = None
     @sio.on("doComputeInitialState")
     async def doComputeInitialState(route: str, page_state: str):
-        print("doComputeInitialState called")
+        printd("doComputeInitialState called")
         try:
             app_dir = paths["app_dir"]
             child_proc = subprocess.Popen(
@@ -68,7 +70,7 @@ def handle_ipc_events(sio, paths):
             print("except", traceback.print_exc())
     @sio.on("doBuildPython")
     async def doBuildPython():
-        print("doBuildPython called")
+        printd("doBuildPython called")
         app_dir = paths["app_dir"]
         controllers_dir = os.path.join(app_dir, "controllers")
         initial_pipfile_path = os.path.join(controllers_dir, "Pipfile")
@@ -81,8 +83,11 @@ def handle_ipc_events(sio, paths):
                     # copy Pipfile to app_dir
                     copy(initial_pipfile_path, final_pipfile_path)
                     # run pipenv install
-                    child_proc = install_with_pipenv(app_dir)
-                    await child_proc.wait()
+                    child_proc = await install_with_pipenv(app_dir)
+                    _, stderr = await child_proc.communicate()
+                    if stderr:
+                        print("Failed: pipenv install")
+                        printd("[stderr]\n", stderr)
                     # delete Pipfile from controllers_dir
                     os.remove(initial_pipfile_path)
                 else:
@@ -95,33 +100,44 @@ def handle_ipc_events(sio, paths):
                     pkgs = pipfile_data["packages"]
                     dev_pkgs = pipfile_data["dev-packages"]
                     # run pipenv install <package_name> for each file in Pipfile inside app_dir
-                    child_procs_wait = []
                     for pkg in pkgs:
                         version = pkgs[pkg]
                         if type(version) != str:
                             version = version["version"]
                         child_proc = await install_with_pipenv(app_dir, pkg, version)
-                        child_procs_wait.append(child_proc.wait())
+                        _, stderr = await child_proc.communicate()
+                        if stderr:
+                            print("Failed: pipenv install ", pkg, version)
+                            printd("[stderr]\n", stderr)
                     for pkg in dev_pkgs:
                         version = dev_pkgs[pkg]
                         if type(version) != str:
                             version = version["version"]
                         child_proc = await install_with_pipenv(app_dir, pkg, version)
-                        child_procs_wait.append(child_proc.wait())
-                    await asyncio.wait(child_procs_wait)
+                        _, stderr = await child_proc.communicate()
+                        if stderr:
+                            print("Failed: pipenv install ", pkg, version)
+                            printd("[stderr]\n", stderr)
                     # delete Pipfile from controllers_dir
                     os.remove(initial_pipfile_path)
                 else:
-                    print("Failed to detect virtual env type. Currently supported are pipenv")
+                    print("Failed to detect virtual env type.",
+                    "Currently supported are virtual envs are - pipenv")
         return True
     @sio.on("doStartPythonServer")
     async def doStartPythonServer():
-        print("doStartPythonServer called")
+        printd("doStartPythonServer called")
         nonlocal python_server_proc
         app_dir = paths["app_dir"]
         if python_server_proc == None:
-            python_server_proc = await run_shell_cmd("python -m controllers.server serve", app_dir)
+            python_server_proc = await run_shell_cmd(
+                "python -m controllers.server serve",
+                app_dir,
+                not globals["in_debug_mode"]
+                )
             await python_server_proc.wait()
+            if python_server_proc.returncode != 0:
+                print("Error occured while running python -m controllers.server serve")
             python_server_proc = None
 async def start_ipc_connection(port: str, app_dir):
     abs_app_dir = os.path.abspath(app_dir)
--- python-packages/atri/src/atri/utils/run_shell_cmd.py
+++ python-packages/atri/src/atri/utils/run_shell_cmd.py
@@ -1,10 +1,10 @@
 import asyncio
 
-async def run_shell_cmd(cmd: str, cwd: str):
+async def run_shell_cmd(cmd: str, cwd: str, pipe: bool = True):
     proc = await asyncio.create_subprocess_shell(
         cmd,
-        stdout=None,
-        stderr=None,
+        stdout=asyncio.subprocess.PIPE if pipe else None,
+        stderr=asyncio.subprocess.PIPE if pipe else None,
         cwd=cwd
         )
     return proc
