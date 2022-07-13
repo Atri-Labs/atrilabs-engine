94a9503eaa629c6f67c7ecbe726bd3ff1f4eda5b
aditya <aditya29ks@gmail.com>
2022-07-13 10:15:30 +0530

    wip: ColorPicker

--- packages/component-style-layer/src/components/commons/ColorPickerAsset.tsx
+++ packages/component-style-layer/src/components/commons/ColorPickerAsset.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React from "react";
 import { CssProprtyComponentType } from "../../types";
 import { ColorPicker, useColor } from "react-color-palette";
 import "react-color-palette/lib/css/styles.css";
@@ -10,6 +10,7 @@ export type ColorPickerProps = {
 };
 
 export const ColorPickerAsset: React.FC<ColorPickerProps> = (props) => {
+  //Internal state is being used to record the last color dragged to in the palette.
   const [color, setColor] = useColor("hex", "");
 
   const handleChange =(
