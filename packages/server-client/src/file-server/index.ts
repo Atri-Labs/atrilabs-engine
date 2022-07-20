e176f3f817e4786345308d2bd9bb2fdae89442c7
cruxcode <swaroopshyam0@gmail.com>
2022-07-20 10:47:14 +0530

    file-server serves static assets

--- packages/server-client/src/file-server/index.ts
+++ packages/server-client/src/file-server/index.ts
@@ -1,15 +1,20 @@
 import { ToolConfig } from "@atrilabs/core";
-import { exec } from "child_process";
 import path from "path";
 import fs from "fs";
 import { exit } from "process";
+import http from "http";
+import express from "express";
+
+const app = express();
+const server = http.createServer(app);
 
 export type FileServerOptions = {
   dir: string;
   port?: number;
 };
 
-export default function (_toolConfig: ToolConfig, options: FileServerOptions) {
+export default function (toolConfig: ToolConfig, options: FileServerOptions) {
+  // pre flight checks
   if (!options || !options.dir) {
     console.log("Required option dir is missing");
     exit();
@@ -22,24 +27,15 @@ export default function (_toolConfig: ToolConfig, options: FileServerOptions) {
     console.log(`[file-server] Directory Not Found: ${options.dir}`);
     exit();
   }
+  // serve static assets
+  const assetUrlPrefix = toolConfig.assetManager.urlPath;
+  const assetsDir = toolConfig.assetManager.assetsDir;
+  app.use(assetUrlPrefix, express.static(assetsDir));
+
+  app.use(express.static(options.dir));
+
   const port = (options && options.port) || 4002;
-  const exe = path.resolve(
-    path.dirname(require.resolve("http-server/package.json")),
-    "..",
-    ".bin",
-    "http-server"
-  );
-  exec(`${exe} ${options.dir} -p ${port} --cors`, (err, stdout, stderr) => {
-    if (err) {
-      console.log(`err:\n${err}`);
-    }
-    if (stderr) {
-      console.log(stderr);
-    }
-    if (stdout) {
-      console.log(stdout);
-    }
+  server.listen(port, "0.0.0.0", () => {
+    console.log(`[file-server] listening on http://0.0.0.0:${port}`);
   });
-  // The default address used by http-server is 0.0.0.0
-  console.log(`[file-server] listening on http://0.0.0.0:${port}`);
 }
