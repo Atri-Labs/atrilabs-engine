df8793b3494dc742db017fb2723eb3f65c19b555
cruxcode <swaroopshyam0@gmail.com>
2022-07-23 15:34:40 +0530

    app-template-layer created

---
+++ packages/app-template-layer/src/layer.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  modulePath: "./index",
+  requires: {
+    menu: {
+      BaseHeaderMenu: "BaseHeaderMenu",
+      BaseFooterMenu: "BaseFooterMenu",
+    },
+    containers: {
+      Logo: "Logo",
+      BaseContainer: "BaseContainer",
+      OverlayContainer: "OverlayContainer",
+    },
+  },
+  exposes: {},
+  decorators: [],
+};
