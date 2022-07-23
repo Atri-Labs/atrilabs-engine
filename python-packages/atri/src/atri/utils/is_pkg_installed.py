67caac17fc1b5cd638bece965fb74cea36b2185f
cruxcode <swaroopshyam0@gmail.com>
2022-07-23 17:50:40 +0530

    fix: conda env list is generated only if conda is installed

--- python-packages/atri/src/atri/utils/is_pkg_installed.py
+++ python-packages/atri/src/atri/utils/is_pkg_installed.py
@@ -1,3 +1,4 @@
+import subprocess
 from .run_shell_cmd import run_shell_cmd
 import os
 from .globals import globals
@@ -15,6 +16,13 @@ async def is_pipenv_installed():
 async def is_conda_installed():
     child_proc = await run_shell_cmd("conda --version", os.getcwd(), globals["in_debug_mode"])
     await child_proc.wait()
+    if child_proc.returncode == 0:
+        return True
+    return False
+
+def is_conda_installed_sync() -> bool:
+    child_proc = subprocess.Popen(["conda", "--version"], stderr=subprocess.PIPE, stdout=subprocess.PIPE, stdin=subprocess.DEVNULL)
+    child_proc.wait()
     if child_proc.returncode == 0:
         return True
     return False
