4cd2cfa830eb3e70a675fc1bc8d7c10dd77c3656
cruxcode <swaroopshyam0@gmail.com>
2022-07-29 20:25:14 +0530

    fixes #29

--- packages/canvas-runtime/src/decorators/UnlockCanvasActivityMachineDecorator.tsx
+++ packages/canvas-runtime/src/decorators/UnlockCanvasActivityMachineDecorator.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { canvasComponentStore } from "../CanvasComponentData";
 import { DecoratorProps, DecoratorRenderer } from "../DecoratorRenderer";
 import {
   getCompDropTarget,
@@ -37,6 +38,18 @@ export const UnlockCanvasActivityMachineDecorator: React.FC<DecoratorProps> = (
       newCompRendererdSubscribers.forEach((cb) => {
         cb(props.compId);
       });
+    }
+  }, [props.compId]);
+  // Set properties/attributes that hinder drag-drop on canvas
+  // so that they don't interfer. For ex: user-select, draggable
+  useEffect(() => {
+    if (canvasComponentStore[props.compId]) {
+      const topElement = canvasComponentStore[props.compId].ref.current;
+      if (topElement) {
+        topElement.style.userSelect = "none";
+        topElement.setAttribute("draggable", "false");
+        topElement.tabIndex = 0;
+      }
     }
   }, [props.compId]);
   return <DecoratorRenderer {...props} />;
