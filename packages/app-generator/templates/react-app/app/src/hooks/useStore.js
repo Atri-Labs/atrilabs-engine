5ec7cfd5ecc4adb86e5b8e58707b4599629a9e45
cruxcode <swaroopshyam0@gmail.com>
2022-08-06 04:53:25 +0530

    wip: react template changed

--- packages/app-generator/templates/react-app/app/src/hooks/useStore.js
+++ packages/app-generator/templates/react-app/app/src/hooks/useStore.js
@@ -1,7 +1,55 @@
 import create from "zustand";
 
 const useStore = create((set) => {
-  // DATA CURSOR
+  return {};
 });
+
+const desktopModeProps = {
+  /* DATA CURSOR */
+};
+
+const breakpointProps = {};
+
+function getViewportDimension() {
+  const width = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+  );
+  const height = Math.max(
+    document.documentElement.clientHeight || 0,
+    window.innerHeight || 0
+  );
+  return { width, height };
+}
+
+function getEffectiveBreakpointWidths(pageName, windowWidth) {
+  if (breakpointProps[pageName] === undefined) {
+    return [];
+  }
+  const widths = Object.keys(breakpointProps[pageName]);
+  return widths.filter((curr) => {
+    return curr >= windowWidth;
+  });
+}
+
+function getEffectivePropsForPage(pageName) {
+  const { width } = getViewportDimension();
+  let effectiveProps = JSON.stringify(desktopModeProps[pageName]);
+  const effectiveWidths = getEffectiveBreakpointWidths(pageName, width);
+  effectiveWidths.forEach((effectiveWidth) => {
+    effectiveProps = {
+      ...effectiveProps,
+      ...breakpointProps[pageName][effectiveWidth],
+    };
+  });
+  return effectiveProps;
+}
+
+export function setEffectivePropsForPage(pageName) {
+  const effectiveProps = getEffectivePropsForPage(pageName);
+  useStore.setState({ [pageName]: effectiveProps });
+}
+
+useStore.setState(desktopModeProps);
 
 export default useStore;
--- packages/app-generator/templates/react-app/app/src/pages/Example.jsx
+++ packages/app-generator/templates/react-app/app/src/pages/Example.jsx
@@ -1,8 +1,25 @@
-import useStore from "../hooks/useStore";
+import { useLayoutEffect } from "react";
+import { useEffect } from "react";
+import useStore, { setEffectivePropsForPage } from "../hooks/useStore";
 // IMPORT CURSOR
 
 export default function Example() {
+  useLayoutEffect(()=>{
+    setEffectivePropsForPage(/* PAGE NAME 1 CURSOR */)
+  }, [])
+
+  useEffect(()=>{
+    const onResize = ()=>{
+      setEffectivePropsForPage(/* PAGE NAME 2 CURSOR */)
+    };
+    window.addEventListener("resize", onResize);
+    return ()=>{
+      window.removeEventListener("resize", onResize);
+    }
+  }, [])
+
   // COMPONENT CURSOR
+
   return (<>
   {/* JSX CURSOR */}
   </>);
