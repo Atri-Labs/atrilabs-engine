e9eae7f62e8eb6dbfd537c57b5dcc863ac994eb8
cruxcode <swaroopshyam0@gmail.com>
2022-08-06 03:47:44 +0530

    getEffectiveStyle merges breakpoint styles

--- packages/canvas-runtime-utils/src/index.ts
+++ packages/canvas-runtime-utils/src/index.ts
@@ -1,1 +1,2 @@
 export * from "./computeBodyChildIndex";
+export * from "./utils";
--- packages/canvas-runtime-utils/src/utils.ts
+++ packages/canvas-runtime-utils/src/utils.ts
@@ -1,4 +1,4 @@
-import { ComponentCoordsWM } from "@atrilabs/canvas-runtime";
+import { Breakpoint, ComponentCoordsWM } from "@atrilabs/canvas-runtime";
 
 // ================== body ===============================
 export function lrtbSort(coords: ComponentCoordsWM[]) {
@@ -10,4 +10,37 @@ export function lrtbSort(coords: ComponentCoordsWM[]) {
     return topDiff;
   });
   return coords;
+}
+
+export function getEffectiveBreakpointWidths(
+  canvasBreakpoint: Breakpoint,
+  breakpoints: { [maxWidth: string]: { property: { styles: any } } }
+) {
+  const effectiveBreapointWidths = Object.keys(breakpoints).filter(
+    (currMax) => {
+      return parseInt(currMax) >= canvasBreakpoint.max;
+    }
+  );
+  return effectiveBreapointWidths.sort((a, b) => {
+    return parseInt(a) - parseInt(b);
+  });
+}
+
+export function getEffectiveStyle(
+  canvasBreakpoint: Breakpoint,
+  breakpoints: { [maxWidth: string]: { property: { styles: any } } },
+  styles: any
+) {
+  const effectiveBreakpointWidths = getEffectiveBreakpointWidths(
+    canvasBreakpoint,
+    breakpoints
+  );
+  let effectiveStyle = { ...styles };
+  effectiveBreakpointWidths.forEach((curr) => {
+    effectiveStyle = {
+      ...effectiveStyle,
+      ...breakpoints[curr].property.styles,
+    };
+  });
+  return effectiveStyle;
 }
