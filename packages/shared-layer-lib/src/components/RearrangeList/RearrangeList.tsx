adb2a36383874d5d3150fbbe0f2a15231b1ad632
cruxcode <swaroopshyam0@gmail.com>
2022-08-05 07:59:38 +0530

    RearrangeList added to shared layer lib

---
+++ packages/shared-layer-lib/src/components/RearrangeList/RearrangeList.tsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useMemo, useRef, useState } from "react";
+
+export type RearrangeListProps = {
+  iconItem: React.ReactNode;
+  items: { node: React.ReactNode; key: string }[];
+  onReposition: (deleteAt: number, insertAt: number) => void;
+};
+
+export const RearrangeList: React.FC<RearrangeListProps> = (props) => {
+  const outerDivRef = useRef<HTMLDivElement | null>(null);
+  const [mouseDownIndex, setMouseDownIndex] = useState<number | null>(null);
+  const [currentMouseIndex, setCurrentMouseIndex] = useState<number | null>(
+    null
+  );
+
+  const itemsClone = useMemo(() => {
+    const itemClone = [...props.items];
+    if (
+      mouseDownIndex !== null &&
+      currentMouseIndex !== null &&
+      mouseDownIndex !== currentMouseIndex
+    ) {
+      const mouseDownItem = itemClone.splice(mouseDownIndex, 1)[0];
+      itemClone.splice(currentMouseIndex, 0, mouseDownItem);
+    }
+    return itemClone;
+  }, [props, mouseDownIndex, currentMouseIndex]);
+
+  useEffect(() => {
+    // mouse leave on outer div
+    // mouse up on outer div
+    // mouse move on outer div
+    if (mouseDownIndex !== null && outerDivRef.current) {
+      const outerEl = outerDivRef.current;
+      const { top, height } = outerEl.getBoundingClientRect();
+      const numItems = props.items.length;
+
+      const onMouseMove = (e: MouseEvent) => {
+        const currentMouseIndex =
+          Math.ceil(e.clientY - top) / (height / numItems);
+        setCurrentMouseIndex(currentMouseIndex);
+      };
+      const onMouseUpOrLeave = (e: MouseEvent) => {
+        // TODO: call callbacks
+        if (currentMouseIndex !== null)
+          props.onReposition(mouseDownIndex, currentMouseIndex);
+        setCurrentMouseIndex(null);
+        setMouseDownIndex(null);
+      };
+      outerEl.addEventListener("mousemove", onMouseMove);
+      outerEl.addEventListener("mouseleave", onMouseUpOrLeave);
+      outerEl.addEventListener("mouseup", onMouseUpOrLeave);
+      return () => {
+        outerEl.removeEventListener("mousemove", onMouseMove);
+        outerEl.removeEventListener("mouseleave", onMouseUpOrLeave);
+        outerEl.removeEventListener("mouseup", onMouseUpOrLeave);
+      };
+    }
+  }, [mouseDownIndex, props, currentMouseIndex]);
+  return (
+    <div ref={outerDivRef} style={{ display: "flex", flexDirection: "column" }}>
+      {itemsClone.map((item, index) => {
+        return (
+          <div style={{ display: "flex", flexDirection: "row" }} key={item.key}>
+            <div
+              style={{
+                cursor: mouseDownIndex !== null ? "grabbing" : "grab",
+                userSelect: "none",
+              }}
+              onMouseDown={() => {
+                setMouseDownIndex(index);
+              }}
+            >
+              Icon
+            </div>
+            <div>{item.node}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
