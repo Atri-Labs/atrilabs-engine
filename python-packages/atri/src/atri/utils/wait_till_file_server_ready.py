ec781ac6520181a56560f036dfe779be38a9a98b
cruxcode <swaroopshyam0@gmail.com>
2022-08-15 16:55:33 +0530

    clear line for Please wait message.

--- python-packages/atri/src/atri/utils/wait_till_file_server_ready.py
+++ python-packages/atri/src/atri/utils/wait_till_file_server_ready.py
@@ -1,11 +1,13 @@
 import requests
 import asyncio
+import sys
 
 async def wait_till_file_server_ready(file_server_port: int):
     while True:
         try:
             requests.get("http://localhost:" + str(file_server_port))
+            sys.stdout.write("\n")
             break
         except:
-            print("file server not ready")
+            sys.stdout.write("\33[2K\rPlease wait while we get the server ready...")
             await asyncio.sleep(0.2)
