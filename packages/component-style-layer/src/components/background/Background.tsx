121527a4f7bcb0702ecfc347dfe1fe057447111d
aditya <aditya29ks@gmail.com>
2022-08-19 03:33:48 +0530

    wip: Background Panel

--- packages/component-style-layer/src/components/background/Background.tsx
+++ packages/component-style-layer/src/components/background/Background.tsx
@@ -11,6 +11,12 @@ import { CssProprtyComponentType } from "../../types";
 import { ReactComponent as DropDownArrow } from "../../assets/layout-parent/dropdown-icon.svg";
 import { Input } from "../commons/Input";
 import { AssetInputButton } from "@atrilabs/shared-layer-lib";
+import { SizeInputWithUnits } from "../commons/SizeInputWithUnits";
+import { ReactComponent as OFA } from "../../assets/size/Auto.svg";
+import { ReactComponent as OFH } from "../../assets/size/overflow-hidden.svg";
+import { ReactComponent as OFS } from "../../assets/size/overflow-scroll.svg";
+import { ReactComponent as OFV } from "../../assets/size/overflow-visible.svg";
+import PropertyRender from "../commons/PropertyRender";
 
 const styles: { [key: string]: React.CSSProperties } = {
   container: {
@@ -56,13 +62,29 @@ const styles: { [key: string]: React.CSSProperties } = {
     ...smallText,
     color: gray400,
     display: "grid",
-    gridTemplateColumns: "15px 60px 15px 60px",
-    rowGap: "20px",
-    textAlign: "center",
+    gridTemplateColumns: "60px 60px 60px",
+    textAlign: "left",
     columnGap: "15px",
     marginBottom: "25px",
   },
 };
+const backgroundSizeValues = ["custom", "cover", "contain"];
+const backgroundRepeatValues = [
+  "repeat",
+  "repeat-x",
+  "repeat-y",
+  "space",
+  "round",
+  "no-repeat",
+];
+const backgroundAttachmentValues = ["fixed", "scroll", "local"];
+const backgroundOriginValues = ["content-box", "padding-box", "border-box"];
+const backgroundClipValues = [
+  "content-box",
+  "padding-box",
+  "border-box",
+  "text",
+];
 
 export const Background: React.FC<CssProprtyComponentType> = (props) => {
   const [showProperties, setShowProperties] = useState(true);
@@ -92,7 +114,7 @@ export const Background: React.FC<CssProprtyComponentType> = (props) => {
       <div
         style={
           showProperties
-            ? { display: "flex", rowGap: "20px", flexDirection: "column" }
+            ? { display: "flex", rowGap: "10px", flexDirection: "column" }
             : { display: "none" }
         }
       >
@@ -105,6 +127,88 @@ export const Background: React.FC<CssProprtyComponentType> = (props) => {
             onClearClick={onBackgroundImageClearClickCb}
           />
         </div>
+        <div style={styles.gridContainer}>
+          <div>&nbsp;</div>
+          <div>Top</div>
+          <div>Right</div>
+          <div style={styles.optionName}>Position</div>
+          <div>
+            <SizeInputWithUnits
+              styleItem="backgroundPositionY"
+              styles={props.styles}
+              patchCb={props.patchCb}
+              defaultValue=""
+            />
+          </div>
+          <div>
+            <SizeInputWithUnits
+              styleItem="backgroundPositionX"
+              styles={props.styles}
+              patchCb={props.patchCb}
+              defaultValue=""
+            />
+          </div>
+        </div>
+        <PropertyRender
+          styleItem="backgroundSize"
+          styleText="Size"
+          styleArray={backgroundSizeValues}
+          patchCb={props.patchCb}
+          styles={props.styles}
+        >
+          <OFV />
+          <OFS />
+          <OFH />
+        </PropertyRender>
+        <PropertyRender
+          styleItem="backgroundRepeat"
+          styleText="Repeat"
+          styleArray={backgroundRepeatValues}
+          patchCb={props.patchCb}
+          styles={props.styles}
+        >
+          <OFV />
+          <OFS />
+          <OFH />
+          <OFV />
+          <OFS />
+          <OFH />
+        </PropertyRender>
+        <PropertyRender
+          styleItem="backgroundAttachment"
+          styleText="Attach"
+          styleArray={backgroundAttachmentValues}
+          patchCb={props.patchCb}
+          styles={props.styles}
+        >
+          <OFV />
+          <OFS />
+          <OFH />
+        </PropertyRender>
+        <PropertyRender
+          styleItem="backgroundOrigin"
+          styleText="Origin"
+          styleArray={backgroundOriginValues}
+          patchCb={props.patchCb}
+          styles={props.styles}
+        >
+          <OFV />
+          <OFS />
+          <OFH />
+        </PropertyRender>
+        <PropertyRender
+          styleItem="backgroundClip"
+          styleText="Cip"
+          styleArray={backgroundClipValues}
+          patchCb={props.patchCb}
+          styles={props.styles}
+        >
+          <OFV />
+          <OFS />
+          <OFH />
+          <OFS />
+        </PropertyRender>
+
         {/**Background Color */}
         <div style={{ display: "flex", alignItems: "center" }}>
           <span style={styles.optionName}>Color</span>
