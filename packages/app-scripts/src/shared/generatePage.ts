4e53147aa781e1468a04d6062b23f29145e45d0a
cruxcode <swaroopshyam0@gmail.com>
2022-09-05 09:40:59 +0530

    bugfix: require path should be from cwd

--- packages/app-scripts/src/shared/generatePage.ts
+++ packages/app-scripts/src/shared/generatePage.ts
@@ -7,9 +7,9 @@ export function generatePage(pageRoute: string, options: GeneratePageOptions) {
   if (options?.reload) {
     delete require.cache[options.paths.getAppText];
   }
-  const getAppText = require(options.paths.getAppText)["getAppText"][
+  const getAppText = require(path.resolve(options.paths.getAppText))[
     "getAppText"
-  ];
+  ]["getAppText"];
   const appHtmlContent = fs.readFileSync(options.paths.appDistHtml).toString();
   const finalText = getAppText(pageRoute, appHtmlContent);
   return finalText;
@@ -29,9 +29,10 @@ export function buildPages(
   const pageRoutes = Object.keys(serverInfo.pages);
   pageRoutes.forEach((pageRoute) => {
     const finalText = generatePage(pageRoute, options);
-    fs.writeFileSync(
-      path.join(options.outputDir, pageRoute, "index.html"),
-      finalText
-    );
+    const dir = path.join(options.outputDir, pageRoute);
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });
+    }
+    fs.writeFileSync(path.join(dir, "index.html"), finalText);
   });
 }
