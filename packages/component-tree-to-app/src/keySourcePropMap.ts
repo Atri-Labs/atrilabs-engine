a18f13fde00b67423b5420e80916462a6c75fbb8
cruxcode <swaroopshyam0@gmail.com>
2022-09-08 10:10:14 +0530

    replace url with assetUrlPrefix in css & custom tree

---
+++ packages/component-tree-to-app/src/keySourcePropMap.ts
@@ -0,0 +1,12 @@
+// To be only used with custom tree
+export const keySourcePropMap: any = {
+  Image: {
+    custom: { src: "static_asset" },
+  },
+  Carousel: {
+    custom: { imageItems: "array_static_asset" },
+  },
+  Menu: {
+    custom: { src: "static_asset" },
+  },
+};
