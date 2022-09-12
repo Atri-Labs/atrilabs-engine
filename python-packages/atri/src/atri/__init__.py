f101a2349b2b00e70d49777742ae1f5a06f4c9d3
cruxcode <swaroopshyam0@gmail.com>
2022-09-12 10:07:03 +0530

    __version__ set to 0.0.58

--- python-packages/atri/src/atri/__init__.py
+++ python-packages/atri/src/atri/__init__.py
@@ -1,6 +1,6 @@
 # __version__ is used in docker run command to identify
 # the version of docker image to run
-__version__ = "0.0.57"
+__version__ = "0.0.58"
 
 supported_virt_types = ["pipenv", "conda"]
 
