018d79dae91841400df4b0c5a65ad3dbbf58010e
cruxcode <swaroopshyam0@gmail.com>
2022-09-12 12:59:32 +0530

    build-ssg builds app & server first

--- packages/app-scripts/src/shared/utils.ts
+++ packages/app-scripts/src/shared/utils.ts
@@ -1,7 +1,8 @@
-import { Configuration, webpack } from "webpack";
+import { Configuration, Stats, webpack } from "webpack";
 import createServerWebpackConfig from "./server.webpack.config";
 import createWebpackConfig from "./webpack.config";
 import path from "path";
+import fs from "fs";
 
 export const imageInlineSizeLimit = parseInt(
   process.env["IMAGE_INLINE_SIZE_LIMIT"] || "10000"
@@ -145,3 +146,122 @@ export const serverInfoFilename = "atri-server-info.json";
 export const serverInfoFile = path.resolve(serverInfoFilename);
 
 export const ssgOutputPath = path.resolve("dist", "ssg");
+
+export function buildAppWithDefaults() {
+  return new Promise<Stats>((res, rej) => {
+    const mode = getMode();
+    setNodeAndBabelEnv(mode);
+
+    try {
+      const buildInfo = JSON.parse(fs.readFileSync(buildInfoFile).toString());
+      if (buildInfo) {
+        const appEntry: string = buildInfo["appEntry"];
+        const appHtml: string = buildInfo["appHtml"];
+        const appOutput: string = buildInfo["appOutput"];
+        const appSrc: string = buildInfo["appSrc"];
+        const manifestDirs: { pkg: string }[] = buildInfo["manifestDirs"];
+        if (
+          !(
+            appEntry &&
+            typeof appEntry === "string" &&
+            appHtml &&
+            typeof appHtml === "string" &&
+            appOutput &&
+            typeof appOutput === "string" &&
+            appSrc &&
+            typeof appSrc === "string" &&
+            manifestDirs &&
+            Array.isArray(manifestDirs)
+          )
+        ) {
+          throw Error(`Wrong schema of ${buildInfoFilename}.`);
+        }
+        const manifestPkgs = manifestDirs.map((manifestDef) => {
+          if (manifestDef["pkg"]) {
+            return path.dirname(
+              require.resolve(`${manifestDef.pkg}/package.json`)
+            );
+          } else {
+            throw Error(`Wrong schema of ${buildInfoFilename}.`);
+          }
+        });
+        const includes = [...manifestPkgs, appSrc];
+        const compiler = buildApp({
+          appEntry: path.resolve(appEntry),
+          appHtml: path.resolve(appHtml),
+          appOutput: path.resolve(appOutput),
+          includes: includes.map((inc) => path.resolve(inc)),
+          mode,
+          addWatchOptions: false,
+          assetUrlPrefix:
+            process.env["ASSET_URL_PREFIX"] || buildInfo.assetUrlPrefix,
+        });
+        compiler.hooks.done.tap("build with defaults", (stats) => {
+          res(stats);
+        });
+      } else {
+        rej(`Missing manifestDirs in ${buildInfoFilename}`);
+      }
+    } catch (err) {
+      rej(err);
+    }
+  });
+}
+
+export function buildServerWithDefaults() {
+  return new Promise<Stats>((res, rej) => {
+    const mode = getMode();
+    setNodeAndBabelEnv(mode);
+
+    // Where will I get manifest directories to include?
+    try {
+      const buildInfo = JSON.parse(fs.readFileSync(buildInfoFile).toString());
+      if (buildInfo) {
+        const serverEntry: string = buildInfo["serverEntry"];
+        const serverSideEntry: string = buildInfo["serverSideEntry"];
+        const serverSrc: string = buildInfo["serverSrc"];
+        const serverOutput: string = buildInfo["serverOutput"];
+        const appSrc: string = buildInfo["appSrc"];
+        const manifestDirs: { pkg: string }[] = buildInfo["manifestDirs"];
+        if (
+          !(
+            serverEntry &&
+            typeof serverEntry === "string" &&
+            serverOutput &&
+            typeof serverOutput === "string" &&
+            serverSrc &&
+            typeof serverSrc === "string"
+          )
+        ) {
+          throw Error(`Wrong schema of ${buildInfoFilename}.`);
+        }
+        const manifestPkgs = manifestDirs.map((manifestDef) => {
+          if (manifestDef["pkg"]) {
+            return path.dirname(
+              require.resolve(`${manifestDef.pkg}/package.json`)
+            );
+          } else {
+            throw Error(`Wrong schema of ${buildInfoFilename}.`);
+          }
+        });
+        const includes = [...manifestPkgs, appSrc, serverSrc];
+        const compiler = buildServer({
+          serverEntry: path.resolve(serverEntry),
+          serverOutput: path.resolve(serverOutput),
+          includes: includes.map((inc) => path.resolve(inc)),
+          mode,
+          allowList: manifestDirs.map((dir) => dir.pkg),
+          addWatchOptions: false,
+          serverSideEntry: path.resolve(serverSideEntry),
+        });
+        compiler.hooks.done.tap("server built with defaults", (stats) => {
+          res(stats);
+        });
+      } else {
+        rej(`Missing manifestDirs in ${buildInfoFilename}`);
+      }
+    } catch (err) {
+      rej(err);
+    }
+  });
+}
--- packages/app-scripts/src/tasks/build-ssg.ts
+++ packages/app-scripts/src/tasks/build-ssg.ts
@@ -6,27 +6,44 @@ import {
 import fs from "fs";
 import path from "path";
 import type { ServerInfo } from "@atrilabs/core";
-import { ssgOutputPath } from "../shared/utils";
+import {
+  ssgOutputPath,
+  buildAppWithDefaults,
+  buildServerWithDefaults,
+} from "../shared/utils";
 
 const appServerInfo: ServerInfo = JSON.parse(
   fs.readFileSync("atri-server-info.json").toString()
 );
 
-import "./build-app";
+const promises = Promise.all([
+  buildAppWithDefaults(),
+  buildServerWithDefaults(),
+]);
 
-import "./build-server";
+promises
+  .then(() => {
+    const outputDir = ssgOutputPath;
 
-const outputDir = ssgOutputPath;
+    // copy app/static directory
+    copyPublicDirectory(appServerInfo, outputDir);
 
-// copy app/static directory
-copyPublicDirectory(appServerInfo, outputDir);
+    buildPages(appServerInfo, {
+      paths: {
+        appDistHtml: path.join(appServerInfo.publicDir, "index.html"),
+        getAppText: path.join(
+          "dist",
+          "app-node",
+          "static",
+          "js",
+          "app.bundle.js"
+        ),
+      },
+      outputDir,
+    });
 
-buildPages(appServerInfo, {
-  paths: {
-    appDistHtml: path.join(appServerInfo.publicDir, "index.html"),
-    getAppText: path.join("dist", "app-node", "static", "js", "app.bundle.js"),
-  },
-  outputDir,
-});
-
-copyAssets(appServerInfo, outputDir);
+    copyAssets(appServerInfo, outputDir);
+  })
+  .catch((err) => {
+    console.log("Error\n", err);
+  });
