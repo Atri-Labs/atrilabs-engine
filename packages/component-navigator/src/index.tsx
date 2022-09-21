334be6fbc1a938e4b612063765b3d0240c7d4d30
cruxcode <swaroopshyam0@gmail.com>
2022-09-21 09:41:41 +0530

    initial setup for component navigator done

--- packages/component-navigator/src/index.tsx
+++ packages/component-navigator/src/index.tsx
@@ -1,3 +1,84 @@
+import { Container, Menu } from "@atrilabs/core";
+import {
+  gray300,
+  gray700,
+  gray800,
+  h1Heading,
+  IconMenu,
+} from "@atrilabs/design-system";
+import { useCallback, useState } from "react";
+import { ReactComponent as CompNavIcon } from "./assets/comp-nav-icon.svg";
+import { Cross } from "./assets/Cross";
+const styles: { [key: string]: React.CSSProperties } = {
+  iconContainer: {
+    borderRight: `1px solid ${gray800}`,
+  },
+  dropContainerItem: {
+    width: "15rem",
+    height: `100%`,
+    backgroundColor: gray700,
+    boxSizing: "border-box",
+    userSelect: "none",
+    overflow: "auto",
+  },
+  dropContainerItemHeader: {
+    display: "flex",
+    justifyContent: "space-between",
+    padding: "0.5rem 1rem",
+  },
+  dropContainerItemHeaderH4: {
+    ...h1Heading,
+    color: gray300,
+    margin: "0px",
+  },
+  icons: {
+    display: "flex",
+    alignItems: "center",
+    height: "100%",
+  },
+  iconsSpan: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+    width: "1.3rem",
+  },
+};
+
 export default function () {
-  return <></>;
+  const [showDropPanel, setShowDropContianer] = useState<boolean>(false);
+  const openDropContainer = useCallback(() => {
+    setShowDropContianer(true);
+  }, []);
+  const closeContainer = useCallback(() => {
+    setShowDropContianer(false);
+  }, []);
+
+  return (
+    <>
+      <Menu name="PageMenu" order={4}>
+        <div style={styles.iconContainer}>
+          <IconMenu onClick={openDropContainer} active={false}>
+            <CompNavIcon />
+          </IconMenu>
+        </div>
+      </Menu>
+
+      {showDropPanel ? (
+        <Container name="Drop" onClose={closeContainer}>
+          <div style={styles.dropContainerItem}>
+            <header style={styles.dropContainerItemHeader}>
+              <h4 style={styles.dropContainerItemHeaderH4}>Navigator</h4>
+              <div style={styles.icons}>
+                <span style={styles.iconsSpan} onClick={closeContainer}>
+                  <Cross />
+                </span>
+              </div>
+            </header>
+            <div>{/* Component Navigator Component goes here */}</div>
+          </div>
+        </Container>
+      ) : null}
+    </>
+  );
 }
