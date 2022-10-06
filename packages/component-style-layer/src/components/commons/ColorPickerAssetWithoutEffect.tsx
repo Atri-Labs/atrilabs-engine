05f34d9c083d9c322a51a596fc3c87d8e840c469
aditya <aditya29ks@gmail.com>
2022-10-07 00:58:37 +0530

    wip: boxShadow

--- packages/component-style-layer/src/components/commons/ColorPickerAssetWithoutEffect.tsx
+++ packages/component-style-layer/src/components/commons/ColorPickerAssetWithoutEffect.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { CssProprtyComponentType } from "../../types";
 import { ColorPicker, useColor, toColor } from "react-color-palette";
 import "react-color-palette/lib/css/styles.css";
