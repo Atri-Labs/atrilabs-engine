2389ca891e756ea46b4873312fc4ecde5c4b3eb7
cruxcode <swaroopshyam0@gmail.com>
2022-10-09 06:44:54 +0530

    removed calls for setEffectiveProps

--- packages/app-generator/templates/react-app/app/src/hooks/useStore.js
+++ packages/app-generator/templates/react-app/app/src/hooks/useStore.js
@@ -45,65 +45,6 @@ const breakpointProps = {
   /* DATA 2 CURSOR */
 };
 
-function getViewportDimension() {
-  const width = Math.min(
-    document.documentElement.clientWidth || 0,
-    window.innerWidth || 0
-  );
-  const height = Math.min(
-    document.documentElement.clientHeight || 0,
-    window.innerHeight || 0
-  );
-  return { width, height };
-}
-
-function getEffectiveBreakpointWidths(pageName, windowWidth) {
-  if (breakpointProps[pageName] === undefined) {
-    return [];
-  }
-  const widths = Object.keys(breakpointProps[pageName]);
-  return widths
-    .filter((curr) => {
-      return parseInt(curr) >= windowWidth;
-    })
-    .sort((a, b) => {
-      return parseInt(b) - parseInt(a);
-    });
-}
-
-/**
- *
- * effective props is combination of
- */
-function getEffectivePropsForPage(pageName) {
-  const { width } = getViewportDimension();
-  // effectiveProps initially has local changes
-  let effectiveProps = JSON.parse(
-    JSON.stringify(useStore.getState()[pageName])
-  );
-  const effectiveWidths = getEffectiveBreakpointWidths(pageName, width);
-  effectiveWidths.forEach((effectiveWidth) => {
-    const compAliases = Object.keys(breakpointProps[pageName][effectiveWidth]);
-    compAliases.forEach((compAlias) => {
-      const propNames = Object.keys(
-        breakpointProps[pageName][effectiveWidth][compAlias]
-      );
-      propNames.forEach((propName) => {
-        effectiveProps[compAlias][propName] = {
-          ...useStore.getState()[pageName][compAlias][propName],
-          ...breakpointProps[pageName][effectiveWidth][compAlias][propName],
-        };
-      });
-    });
-  });
-  return effectiveProps;
-}
-
-export function setEffectivePropsForPage(pageName) {
-  const effectiveProps = getEffectivePropsForPage(pageName);
-  useStore.getState().setPage(pageName, effectiveProps);
-}
-
 useStore.setState(desktopModeProps);
 
 export default useStore;
