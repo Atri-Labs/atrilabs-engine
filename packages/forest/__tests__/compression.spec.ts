0df499bf19a535f82319ecb96111f5ff159eacf6
cruxcode <swaroopshyam0@gmail.com>
2022-10-12 04:45:33 +0530

    test if linked trees are compressed

--- packages/forest/__tests__/compression.spec.ts
+++ packages/forest/__tests__/compression.spec.ts
@@ -1,4 +1,4 @@
-import { AnyEvent } from "../src/types";
+import { AnyEvent, CreateEvent } from "../src/types";
 import { compressEvents } from "../src/compression";
 import { componentTreeDef, cssTreeDef, forestDef } from "./forestDefExample";
 
@@ -23,6 +23,13 @@ test("patches are merged", () => {
   ];
   const compressedEvents = compressEvents(events, forestDef);
   expect(compressedEvents.length).toBe(1);
+  expect(compressedEvents[0]).toHaveProperty("state");
+  expect((compressedEvents[0]! as CreateEvent)["state"]).toHaveProperty(
+    "alias"
+  );
+  expect((compressedEvents[0]! as CreateEvent)["state"]["alias"]).toBe(
+    "alias2"
+  );
 });
 
 test("create & delete results in no event", () => {
@@ -63,4 +70,48 @@ test("deleting root tree node, results in no event for linked tree", () => {
   ];
   const compressedEvents = compressEvents(events, forestDef);
   expect(compressedEvents.length).toBe(0);
+});
+
+test("css tree is linked with comp tree", () => {
+  const events: AnyEvent[] = [
+    {
+      type: `CREATE$$${componentTreeDef.modulePath}`,
+      meta: {},
+      id: "comp1",
+      state: { parent: { id: "body", index: 0 } },
+    },
+    {
+      type: `PATCH$$${componentTreeDef.modulePath}`,
+      id: "comp1",
+      slice: { alias: "alias1" },
+    },
+    {
+      type: `PATCH$$${componentTreeDef.modulePath}`,
+      id: "comp1",
+      slice: { alias: "alias2" },
+    },
+    {
+      type: `CREATE$$${cssTreeDef.modulePath}`,
+      meta: {},
+      id: "css1",
+      state: {
+        parent: { id: "", index: 0 },
+        property: { styles: { height: "10px" } },
+      },
+    },
+    {
+      type: `LINK$$${cssTreeDef.modulePath}`,
+      childId: "css1",
+      refId: "comp1",
+    },
+  ];
+  const compressedEvents = compressEvents(events, forestDef);
+  expect(compressedEvents.length).toBe(3);
+  expect(compressedEvents[0]!).toHaveProperty("state");
+  expect((compressedEvents[0]! as CreateEvent)["state"]).toBeDefined();
+  expect(
+    (compressedEvents[0]! as CreateEvent)["state"]["property"]["styles"][
+      "height"
+    ]
+  ).toBe("10px");
 });
