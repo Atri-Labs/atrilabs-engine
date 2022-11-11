dacf60d8a49cf8911f41d6c9ae05b35bdf2f800e
Jonathan Alvares <alvaresjonathan599@gmail.com>
2022-11-11 11:38:23 +0530

    Moved common functionality from tab body into a new component

--- packages/custom-props-layer/src/TabBody.tsx
+++ packages/custom-props-layer/src/TabBody.tsx
@@ -1,27 +1,10 @@
 import { gray300, gray800, h1Heading } from "@atrilabs/design-system";
 import React, { useMemo } from "react";
 import { TabBodyProps } from "./types";
-import { Text } from "./components/text/Text";
-import { StaticAsset } from "./components/static-asset/StaticAsset";
-import { Boolean } from "./components/boolean/Boolean";
-import { LargeText } from "./components/large-text/LargeText";
-import { ListField } from "./components/list/ListField";
-import { Number } from "./components/number/Number";
-import { BooleanList } from "./components/boolean-list/BooleanList";
-import { NumberList } from "./components/number-list/NumberList";
-import { StaticAssetList } from "./components/static-asset-list/StaticAssetList";
-import { Color } from "./components/color/Color";
-import { InternalLink } from "./components/internal-link/InternalLink";
-import { usePageRoutes } from "./hooks/usePageRoutes";
-import { ComponentSelector } from "./components/component-selector/ComponentSelector";
-import { ExternalLink } from "./components/external-link/ExternalLink";
-import { Enum } from "./components/enum/Enum";
-import {
-  EnumCustomProp,
-  MapCustomProp,
-} from "@atrilabs/app-design-forest/lib/customPropsTree";
-import { EnumList } from "./components/enum-list/EnumList";
+import { MapCustomProp } from "@atrilabs/app-design-forest/lib/customPropsTree";
 import { Map } from "./components/map/Map";
+import { CommonPropTypeContainer } from "./components/commons/CommonPropTypeContainer";
+import { usePageRoutes } from "./hooks/usePageRoutes";
 
 const styles: { [key: string]: React.CSSProperties } = {
   // top level container
@@ -57,153 +40,6 @@ export const TabBody: React.FC<TabBodyProps> = (props) => {
     <div style={styles.container}>
       {propNames.map((propName) => {
         const propType = props.treeOptions.dataTypes[propName].type;
-        if (propType === "text")
-          return (
-            <Text
-              {...props}
-              propName={propName}
-              key={propName}
-              routes={routes}
-            />
-          );
-        if (propType === "static_asset")
-          return (
-            <StaticAsset
-              {...props}
-              propName={propName}
-              key={propName}
-              routes={routes}
-            />
-          );
-        if (propType === "boolean")
-          return (
-            <Boolean
-              {...props}
-              propName={propName}
-              key={propName}
-              routes={routes}
-            />
-          );
-        if (propType === "large_text")
-          return (
-            <LargeText
-              {...props}
-              propName={propName}
-              key={propName}
-              routes={routes}
-            />
-          );
-        if (propType === "array")
-          return (
-            <ListField
-              {...props}
-              propName={propName}
-              key={propName}
-              routes={routes}
-            />
-          );
-        if (propType === "number")
-          return (
-            <Number
-              {...props}
-              propName={propName}
-              key={propName}
-              routes={routes}
-            />
-          );
-        if (propType === "array_boolean")
-          return (
-            <BooleanList
-              {...props}
-              propName={propName}
-              key={propName}
-              routes={routes}
-            />
-          );
-        if (propType === "array_number")
-          return (
-            <NumberList
-              {...props}
-              propName={propName}
-              key={propName}
-              routes={routes}
-            />
-          );
-        if (propType === "array_static_asset")
-          return (
-            <StaticAssetList
-              {...props}
-              propName={propName}
-              key={propName}
-              routes={routes}
-            />
-          );
-        if (propType === "color")
-          return (
-            <Color
-              {...props}
-              propName={propName}
-              key={propName}
-              routes={routes}
-            />
-          );
-        if (propType === "internal_link")
-          return (
-            <InternalLink
-              {...props}
-              propName={propName}
-              key={propName}
-              routes={routes}
-            />
-          );
-        if (propType === "component_selector")
-          return (
-            <ComponentSelector
-              {...props}
-              propName={propName}
-              key={propName}
-              routes={routes}
-            />
-          );
-        if (propType === "external_link")
-          return (
-            <ExternalLink
-              {...props}
-              propName={propName}
-              key={propName}
-              routes={routes}
-            />
-          );
-        if (propType === "enum") {
-          const enumCustomProps = props.treeOptions.dataTypes[
-            propName
-          ] as EnumCustomProp;
-          const options = enumCustomProps.options;
-          return (
-            <Enum
-              {...props}
-              options={options}
-              propName={propName}
-              key={propName}
-              routes={routes}
-            />
-          );
-        }
-        if (propType === "array_enum") {
-          const enumListCustomProps = props.treeOptions.dataTypes[
-            propName
-          ] as EnumCustomProp;
-          const options = enumListCustomProps.options;
-          return (
-            <EnumList
-              {...props}
-              options={options}
-              propName={propName}
-              key={propName}
-              routes={routes}
-            />
-          );
-        }
         if (propType === "map") {
           const mapCustomProps = props.treeOptions.dataTypes[
             propName
@@ -212,14 +48,21 @@ export const TabBody: React.FC<TabBodyProps> = (props) => {
           return (
             <Map
               {...props}
-              attributes={attributes}
               propName={propName}
               key={propName}
               routes={routes}
             />
           );
         }
-        return <React.Fragment key={propName}></React.Fragment>;
+        return (
+          <CommonPropTypeContainer
+            {...props}
+            propType={propType}
+            propName={propName}
+            key={propName}
+            routes={routes}
+          />
+        );
       })}
     </div>
   );
