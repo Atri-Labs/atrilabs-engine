bb71af1ef0e6cf3dd48460f79b892b81e8fc7168
aditya <aditya29ks@gmail.com>
2022-11-15 15:18:23 +0530

    accordion css changes

--- packages/react-component-manifests/src/manifests/Accordion/Accordion.tsx
+++ packages/react-component-manifests/src/manifests/Accordion/Accordion.tsx
@@ -29,31 +29,83 @@ export const AccordionComponent: React.FC<AccordionComponentTypes> = ({
   const content = useRef<HTMLDivElement>(null);
 
   return (
-    <div className={`${className ? className : ""} accordion-section`}>
-      <button
-        className={`accordion ${open ? "active" : ""}`}
-        onClick={() => {
-          onTitleClick();
-        }}
-      >
-        <Chevron
-          className={`${open ? "accordion-icon rotate" : "accordion-icon"}`}
-          fill={"#777"}
-        />
-        <p className="accordion-title" style={{ marginLeft: "1rem" }}>
-          {title}
-        </p>
-      </button>
-      <div
-        ref={content}
-        style={{
-          height: `${open ? content.current?.scrollHeight + "px" : "0px"}`,
-        }}
-        className="accordion-content"
-      >
-        <div className="accordion-text">{description}</div>
+    <>
+      <style>
+        {`.accordion-section {
+            display: flex;
+            flex-direction: column;
+          }
+
+          .accordion {
+            background-color: #eee;
+            color: #444;
+            cursor: pointer;
+            padding: 18px;
+            display: flex;
+            align-items: center;
+            border: none;
+            outline: none;
+            transition: background-color 0.6s ease;
+          }
+
+          .accordion:hover,
+          .active {
+            background-color: #ccc;
+          }
+          
+          .accordion-title {
+            font-weight: 600;
+            font-size: 14px;
+            text-align: left;
+          }
+          
+          .accordion__icon {
+            margin-left: auto;
+            transition: transform 0.6s ease;
+          }
+          
+          .rotate {
+            transform: rotate(90deg);
+          }
+
+          .accordion-content {
+            background-color: white;
+            overflow: auto;
+          }
+
+          .accordion-text {
+            font-weight: 400;
+            font-size: 14px;
+            padding: 18px;
+          }
+        `}
+      </style>
+      <div className={`${className ? className : ""} accordion-section`}>
+        <button
+          className={`accordion ${open ? "active" : ""}`}
+          onClick={() => {
+            onTitleClick();
+          }}
+        >
+          <Chevron
+            className={`${open ? "accordion-icon rotate" : "accordion-icon"}`}
+            fill={"#777"}
+          />
+          <p className="accordion-title" style={{ marginLeft: "1rem" }}>
+            {title}
+          </p>
+        </button>
+        <div
+          ref={content}
+          style={{
+            height: `${open ? content.current?.scrollHeight + "px" : "0px"}`,
+          }}
+          className="accordion-content"
+        >
+          <div className="accordion-text">{description}</div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
