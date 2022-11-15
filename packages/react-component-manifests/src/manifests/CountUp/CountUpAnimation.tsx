d11f56d1d379d0809d8ea3bd430e8d7fbe709c38
dibkb <dibas9110@gmail.con>
2022-11-15 19:11:36 +0530

    Countup "Infinity" bug solved

--- packages/react-component-manifests/src/manifests/CountUp/CountUpAnimation.tsx
+++ packages/react-component-manifests/src/manifests/CountUp/CountUpAnimation.tsx
@@ -3,26 +3,28 @@ const easeOutQuad = (t: number) => t * (2 - t);
 const frameDuration = 1000 / 60;
 export const CountUpAnimation: React.FC<CountUpAnimationProp> = ({
   value,
-  duration = 2000,
+  duration,
 }) => {
   const [count, setCount] = useState(0);
   useEffect(() => {
     let frame = 0;
-    const totalFrames = Math.round(duration / frameDuration);
-    const counter = setInterval(() => {
-      frame++;
-      const progress = easeOutQuad(frame / totalFrames);
-      setCount(value * progress);
+    const totalFrames: number = Math.round(duration / frameDuration);
+    if (totalFrames !== 0) {
+      const counter = setInterval(() => {
+        frame++;
+        const progress = easeOutQuad(frame / totalFrames);
+        setCount(value * progress);
 
-      if (frame === totalFrames) {
-        clearInterval(counter);
-      }
-    }, frameDuration);
+        if (frame === totalFrames) {
+          clearInterval(counter);
+        }
+      }, frameDuration);
+    }
   }, [value, duration]);
   return <>{Math.floor(count)}</>;
 };
 interface CountUpAnimationProp {
   value: number;
-  duration?: number;
+  duration: number;
 }
 //Reference : https://jshakespeare.com/simple-count-up-number-animation-javascript-react/
