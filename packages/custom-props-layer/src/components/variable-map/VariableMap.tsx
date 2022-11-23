39e4a213757214647a6aba5e452b30c45d2899a3
Jonathan Alvares <alvaresjonathan599@gmail.com>
2022-11-23 07:36:01 +0530

    Created UI for VariableKeyMap

---
+++ packages/custom-props-layer/src/components/variable-map/VariableMap.tsx
@@ -0,0 +1,59 @@
+import { gray900 } from "@atrilabs/design-system";
+import { useCallback, useMemo } from "react";
+import { ComponentProps } from "../../types";
+import { createObject } from "@atrilabs/canvas-runtime-utils/src/utils";
+
+export const VariableMap: React.FC<ComponentProps> = (props) => {
+  const selector = useMemo(() => {
+    return props.selector || [];
+  }, [props]);
+  const propValue = useMemo(() => {
+    let currentValue = props.customProps;
+    for (let prop of selector) {
+      currentValue = currentValue[prop];
+    }
+    return currentValue["selectedOption"] || "";
+  }, [props, selector]);
+  const callPatchCb = useCallback(
+    (e: React.ChangeEvent<HTMLSelectElement>) => {
+      props.patchCb({
+        property: {
+          custom: createObject(
+            props.customProps,
+            [...selector, "selectedOption"],
+            e.target.value
+          ),
+        },
+      });
+    },
+    [props, selector]
+  );
+
+  return (
+    <div>
+      <div
+        style={{ color: "white", paddingBottom: "12px" }}
+      >{`Add a property to ${props.propName}`}</div>
+      <select
+        value={propValue}
+        onChange={callPatchCb}
+        style={{
+          height: "25px",
+          backgroundColor: gray900,
+          border: "none",
+          outline: "none",
+          color: "white",
+          padding: "0 4px",
+          minWidth: "none",
+          width: "100%",
+        }}
+      >
+        {props.attributes!.map((attribute, index) => (
+          <option value={attribute.fieldName} key={index}>
+            {attribute.fieldName}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+};
