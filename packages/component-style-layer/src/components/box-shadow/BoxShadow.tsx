190dd57c3be102063dd0c86fcbae04303cc748af
Jonathan Alvares <alvaresjonathan599@gmail.com>
2022-12-26 18:53:53 +0530

    Implemented the ability for users to show the box shadow property and not

--- packages/component-style-layer/src/components/box-shadow/BoxShadow.tsx
+++ packages/component-style-layer/src/components/box-shadow/BoxShadow.tsx
@@ -1,3 +1,55 @@
-export const BoxShadow: React.FC<any> = (props) => {
-  return <div></div>;
+import { gray200, gray800, h5Heading } from "@atrilabs/design-system";
+import { ReactComponent as DropDownArrow } from "../../assets/layout-parent/dropdown-icon.svg";
+import { CssProprtyComponentType } from "../../types";
+import { useState } from "react";
+
+const styles: { [key: string]: React.CSSProperties } = {
+  container: {
+    display: "flex",
+    flexDirection: "column",
+    paddingLeft: "0.5rem",
+    paddingRight: "0.5rem",
+    paddingTop: "1.2rem",
+    paddingBottom: "1.8rem",
+    borderBottom: `1px solid ${gray800}`,
+    rowGap: "1.2rem",
+  },
+  header: {
+    ...h5Heading,
+    color: gray200,
+    display: "flex",
+    paddingLeft: "0.5rem",
+    userSelect: "none",
+  },
+  drop: {
+    display: "flex",
+    alignItems: "baseline",
+    cursor: "pointer",
+  },
+};
+
+export const BoxShadow: React.FC<CssProprtyComponentType> = (props) => {
+  const [showProperties, setShowProperties] = useState(true);
+  return (
+    <div style={styles.container}>
+      <div style={styles.drop}>
+        <DropDownArrow
+          onClick={() => setShowProperties(!showProperties)}
+          style={
+            !showProperties
+              ? { transform: "rotate(-90deg)" }
+              : { transform: "rotate(0deg)" }
+          }
+        />
+        <div style={styles.header}>Box Shadow</div>
+      </div>
+      <div
+        style={
+          showProperties
+            ? { display: "flex", rowGap: "2rem", flexDirection: "column" }
+            : { display: "none" }
+        }
+      ></div>
+    </div>
+  );
 };
