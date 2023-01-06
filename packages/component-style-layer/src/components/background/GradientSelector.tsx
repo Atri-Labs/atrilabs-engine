3e43215cf125cdeedc0847be8bf4cd8a93d566a1
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-01-06 22:30:47 +0530

    Created gradient data structure to hold color and stop

--- packages/component-style-layer/src/components/background/GradientSelector.tsx
+++ packages/component-style-layer/src/components/background/GradientSelector.tsx
@@ -1,7 +1,7 @@
 import { smallText, gray100, gray800 } from "@atrilabs/design-system";
 import { useRef, useState } from "react";
 import { Cross } from "../../icons/Cross";
-import { ColorPicker, useColor, toColor } from "react-color-palette";
+import { ColorPicker, useColor, toColor, Color } from "react-color-palette";
 
 type GradientColorSelectorTypes = {
   gradient: string;
@@ -9,43 +9,75 @@ type GradientColorSelectorTypes = {
   updateGradient: (gradient: string) => void;
 };
 
+type Position = {
+  stop: number;
+  color: Color;
+};
+
 export const GradientColorSelector = () => {
   const [gradientType, setGradientType] = useState<string>("linearGradient");
 
-  const [positions, setPositions] = useState<number[]>([0, 100]);
-  const [selectedPosition, setSelectedPosition] = useState<number | null>(null);
+  const [positions, setPositions] = useState<Position[]>([
+    { stop: 0, color: toColor("hex", "black") },
+    { stop: 100, color: toColor("hex", "red") },
+  ]);
+  const [selectedPositionIdx, setSelectedPositionIdx] = useState<number>(0);
+  const [color, setColor] = useState(positions ? positions[0].color.hex : "");
   const divRef = useRef<HTMLDivElement>(null);
-
-  const [color, setColor] = useColor("hex", "");
 
   const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
     const { clientX } = event;
     const divRect = divRef.current!.getBoundingClientRect();
-    console.log("Gradient Type divReact", event);
     const x = Math.trunc((Math.trunc(clientX - divRect.left) / 250) * 100);
-    if (x >= 0 && x <= 100) {
-      setSelectedPosition(x);
-      setPositions(Array.from(new Set([...positions, x])));
+    let stopPresent = false;
+    for (let i = 0; i < positions.length; i++) {
+      if (
+        x === positions[i].stop ||
+        (Math.abs(x - positions[i].stop) >= 0 &&
+          Math.abs(x - positions[i].stop) <= 5)
+      ) {
+        stopPresent = true;
+        setSelectedPositionIdx(i);
+        break;
+      }
+    }
+    if (!stopPresent) {
+      setSelectedPositionIdx(positions.length);
+      setPositions([
+        ...positions,
+        { stop: x, color: toColor("hex", color || "") },
+      ]);
     }
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
     if (
-      selectedPosition !== null &&
+      selectedPositionIdx !== null &&
       (event.key === "Backspace" || event.key === "Delete")
     ) {
-      setPositions(
-        positions.filter((position) => position !== selectedPosition)
-      );
-      setSelectedPosition(null);
+      setPositions(positions.filter((_, idx) => idx !== selectedPositionIdx));
+      setSelectedPositionIdx(0);
     }
   };
 
-  console.log("Gradient Type", selectedPosition);
+  const changeColor = (index: number, color: string) => {
+    const value = positions[index];
+    value.color = toColor("hex", color);
+    const values = [...positions];
+    values.splice(index, 1, value);
+    setPositions(values);
+    setColor(toColor("hex", color)["hex"]);
+  };
+
+  console.log("Gradient Type", positions);
   return (
     <>
       <style>
-        {`#gradientBar:focus-visible {outline: none} #gradientType {border: none} #gradientType:hover {border: 1px solid #fff}`}
+        {`
+        #gradientBar:focus-visible {outline: none} 
+        #gradientBar:hover {cursor: crosshair} 
+        #gradientType {border: none} 
+        #gradientType:hover {border: 1px solid #fff}`}
       </style>
       <div
         style={{
@@ -99,16 +131,16 @@ export const GradientColorSelector = () => {
         >
           {positions.map((position, index) => (
             <div
-              key={index * position}
+              key={index + color}
               style={{
                 position: "absolute",
-                left: `${position}%`,
+                left: `${position.stop}%`,
                 top: "-2px",
                 width: "8px",
                 height: "1.2em",
-                backgroundColor: "red",
+                backgroundColor: `${position.color.hex}` || "",
                 borderRadius: "3px",
-                border: position === selectedPosition ? "1px solid black" : "",
+                border: index === selectedPositionIdx ? "1px solid black" : "",
               }}
             />
           ))}
@@ -116,9 +148,13 @@ export const GradientColorSelector = () => {
         <ColorPicker
           width={250}
           height={200}
-          color={color}
-          onChange={(e) => setColor(toColor("hex", e.hex))}
-          onChangeComplete={(e) => setColor(toColor("hex", e.hex))}
+          color={
+            positions[selectedPositionIdx]
+              ? positions[selectedPositionIdx].color
+              : toColor("hex", "")
+          }
+          onChange={(e) => changeColor(selectedPositionIdx, e.hex)}
+          onChangeComplete={(e) => changeColor(selectedPositionIdx, e.hex)}
           hideHSV
           dark
         />
