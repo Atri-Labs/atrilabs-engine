9153ca0106609303e3ad89e3bbb55329bb67a868
cruxcode <swaroopshyam0@gmail.com>
2023-01-17 14:04:04 +0800

    route object to paths transform

--- packages/atri-app-core/__tests__/transformations.spec.ts
+++ packages/atri-app-core/__tests__/transformations.spec.ts
@@ -1,16 +1,27 @@
 import { PathsIR } from "../src/utils/PathsIR";
 import {
   dirStructureToIR,
+  IRToUnixFilePath,
   pathsIRToRouteObjectPaths,
+  routeObjectPathToIR,
 } from "../src/utils/transformations";
 
 test("dir structure is transformed to IR", () => {
-  const unixFilepaths = ["/index.js", "/blogs/index.js"];
+  const unixFilepaths = [
+    "/index.js",
+    "/blogs/index.js",
+    "/careers/$.js",
+    "/products/$id.js",
+  ];
   const ir = dirStructureToIR(unixFilepaths);
-  expect(ir.length).toBe(2);
+  expect(ir.length).toBe(4);
   expect(ir[0][0]).toMatchObject({ name: "index", type: "static" });
   expect(ir[1][0]).toMatchObject({ name: "blogs", type: "static" });
   expect(ir[1][1]).toMatchObject({ name: "index", type: "static" });
+  expect(ir[2][0]).toMatchObject({ name: "careers", type: "static" });
+  expect(ir[2][1]).toMatchObject({ name: "*", type: "catchall" });
+  expect(ir[3][0]).toMatchObject({ name: "products", type: "static" });
+  expect(ir[3][1]).toMatchObject({ name: "id", type: "dynamic" });
 });
 
 test("IR transformed to react router paths", () => {
@@ -30,4 +41,30 @@ test("IR transformed to react router paths", () => {
   expect(routerPaths[0]).toBe("/");
   expect(routerPaths[1]).toBe("/blogs");
   expect(routerPaths[2]).toBe("/careers/:id");
+});
+
+test("route object path to IR", () => {
+  const routeObjectPaths = ["/", "/blogs", "/careers/:id"];
+  const irs = routeObjectPaths.map((routeObjectPath) => {
+    return routeObjectPathToIR(routeObjectPath);
+  });
+  expect(irs[0][0].type).toBe("static");
+  expect(irs[0][0].name).toBe("index");
+  expect(irs[1][0].type).toBe("static");
+  expect(irs[1][0].name).toBe("blogs");
+  expect(irs[2][0].type).toBe("static");
+  expect(irs[2][0].name).toBe("careers");
+  expect(irs[2][1].type).toBe("dynamic");
+  expect(irs[2][1].name).toBe("id");
+});
+
+test("route object to unix file path", () => {
+  const routeObjectPaths = ["/", "/blogs", "/careers/:id"];
+  const unixFilePaths = routeObjectPaths.map((routeObjectPath) => {
+    const ir = routeObjectPathToIR(routeObjectPath);
+    return IRToUnixFilePath(ir);
+  });
+  expect(unixFilePaths[0]).toBe("/index");
+  expect(unixFilePaths[1]).toBe("/blogs");
+  expect(unixFilePaths[2]).toBe("/careers/$id");
 });
--- packages/atri-app-core/src/utils/transformations.ts
+++ packages/atri-app-core/src/utils/transformations.ts
@@ -22,21 +22,15 @@ export function dirStructureToIR(unixFilepaths: string[]) {
       const segmentWithoutExt = isLastSegment
         ? segment.slice(0, -1 * path.extname(segment).length)
         : segment;
-      if (
-        segmentWithoutExt.startsWith("[...") &&
-        segmentWithoutExt.endsWith("]")
-      ) {
+      if (segmentWithoutExt === "$") {
         return {
           type: "catchall",
-          name: segmentWithoutExt.replace(/(^(\[\.\.\.)|(\])$)/, ""),
+          name: "*",
         } as CatchAllSegment;
-      } else if (
-        segmentWithoutExt.startsWith("[") &&
-        segmentWithoutExt.endsWith("]")
-      ) {
+      } else if (segmentWithoutExt.startsWith("$")) {
         return {
           type: "dynamic",
-          name: segmentWithoutExt.replace(/(^(\[)|(\])$)/, ""),
+          name: segmentWithoutExt.replace("$", ""),
         } as DynamicSegment;
       } else {
         return { type: "static", name: segmentWithoutExt } as StaticSegment;
@@ -75,4 +69,38 @@ export function pathsIRToRouteObjectPaths(pathsIR: PathsIR) {
   return pathsIR.map((ir) => {
     return pathIRToRouteObjectPath(ir);
   });
+}
+
+export function routeObjectPathToIR(routeObjectPath: string): PathIR {
+  const segments = routeObjectPath.split("/").slice(1);
+  if (segments.length === 1 && segments[0] === "") {
+    return [{ type: "static", name: "index" }];
+  }
+  return segments.map((segment) => {
+    if (segment.startsWith(":")) {
+      return { type: "dynamic", name: segment.replace(":", "") };
+    }
+    if (segment === "*") {
+      return { type: "catchall", name: "$" };
+    }
+    return { type: "static", name: segment };
+  });
+}
+
+export function IRToUnixFilePath(ir: PathIR) {
+  return (
+    "/" +
+    ir
+      .map((val) => {
+        switch (val.type) {
+          case "static":
+            return val.name;
+          case "dynamic":
+            return `$${val.name}`;
+          case "catchall":
+            return "$";
+        }
+      })
+      .join("/")
+  );
 }
