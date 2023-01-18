49062b80a53e003df13239f414936a4718b21a2f
cruxcode <swaroopshyam0@gmail.com>
2023-01-18 14:58:06 +0800

    bugfix: /pages to pages

--- packages/commands/src/consts.ts
+++ packages/commands/src/consts.ts
@@ -1,3 +1,9 @@
 import path from "path";
 
-export const PAGE_DIR = path.resolve("/pages");
+export const PAGE_DIR = path.resolve("pages");
+
+function resolvePage(relativePath: string) {
+  return path.resolve(PAGE_DIR, relativePath);
+}
+
+export const ERROR_PAGE = resolvePage("error");
