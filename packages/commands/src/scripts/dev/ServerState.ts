7b8bbd2b21b2a6c8e63673ad09889baa0dca297a
cruxcode <swaroopshyam0@gmail.com>
2023-01-24 17:43:57 +0800

    added comments & removed unused file

--- packages/commands/src/scripts/dev/ServerState.ts
+++
@@ -1,9 +0,0 @@
-export class ServerState {
-  // Server is in ready state only after watchers have loaded initial directory
-  // Tracks state of both the compilers
-  // Saves triplet of req, res, next
-  // Does not process and respond to any request while any of the compiler
-  // is not ready.
-  // Responds quickly if none of the compilers are in processing mode and the page is ready
-  // Once the compilers are not in processing mode, call watching.invalidate for requests for new page
-}
--- packages/commands/src/scripts/dev/serverMachine.ts
+++ packages/commands/src/scripts/dev/serverMachine.ts
@@ -184,6 +184,25 @@ function swapRequestReservoir(context: SERVER_MACHINE_CONTEXT) {
   context.requestReservoir = [];
 }
 
+/**
+ * Server is in ready state only after watchers have
+ * loaded initial directory.
+ *
+ * Tracks state of both the compilers.
+ *
+ * Saves triplet of req, res, next.
+ *
+ * Does not process and respond to any request while
+ * any of the compiler is not ready.
+ *
+ * Responds quickly if none of the compilers are in
+ * processing mode and the page is ready.
+ *
+ * Once the compilers are not in processing mode,
+ * call watching.invalidate for requests for new page.
+ * @param id
+ * @returns
+ */
 export function createServerMachine(id: string) {
   return createMachine(
     {
