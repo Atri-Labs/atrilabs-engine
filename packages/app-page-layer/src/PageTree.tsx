9ea2318a3d5b3640ee8897d56d852496e5ee82ca
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-02-08 22:39:58 +0530

    On opening directory select the first file or dont change

--- packages/app-page-layer/src/PageTree.tsx
+++ packages/app-page-layer/src/PageTree.tsx
@@ -1,13 +1,10 @@
 import { useCallback, useMemo } from "react";
 import { PageInfo } from "./types";
 import { canvasApi } from "@atrilabs/pwa-builder-manager";
+import { Tree } from "antd";
+import type { DataNode, DirectoryTreeProps } from "antd/es/tree";
 
-type treeNode = {
-  title: string;
-  key: string;
-  children?: treeNode[];
-  isLeaf?: boolean;
-};
+const { DirectoryTree } = Tree;
 
 export const PageTree: React.FC<{
   pagesInfo: PageInfo;
@@ -15,16 +12,21 @@ export const PageTree: React.FC<{
   onCloseClicked: () => void;
 }> = (props) => {
   const createTreeNodes = useCallback(
-    (pathArr: string[], pathArrLen: number, treeNodes: treeNode[]) => {
+    (
+      pathArr: string[],
+      pathArrLen: number,
+      route: string,
+      treeNodes: DataNode[]
+    ) => {
       const node = {
         title:
           pathArr[pathArrLen - 1] !== "" ? pathArr[pathArrLen - 1] : "index",
         isLeaf: true,
-        key: pathArr[pathArrLen - 1] + " " + pathArrLen,
+        key: route,
       };
       if (pathArrLen === 1) treeNodes.push(node);
       else {
-        let tempTreeNode: treeNode[] = treeNodes;
+        let tempTreeNode: DataNode[] = treeNodes;
         for (let i = 0; i < pathArrLen - 1; i++) {
           const path = pathArr[i];
           let j;
@@ -37,7 +39,7 @@ export const PageTree: React.FC<{
           if (j === treeNodes.length) {
             treeNodes.push({
               title: path !== "" ? path : "index",
-              key: path + " " + pathArrLen,
+              key: route + " " + pathArrLen,
               children: [],
             });
             tempTreeNode = treeNodes[treeNodes.length - 1].children || [];
@@ -50,26 +52,39 @@ export const PageTree: React.FC<{
   );
 
   const treeNodes = useMemo(() => {
-    const treeNodes: treeNode[] = [];
+    const treeNodes: DataNode[] = [];
     for (let i = 0; i < props.pagesInfo.length; i++) {
       const path = props.pagesInfo[i].unixFilepath;
+      const route = props.pagesInfo[i].routeObjectPath;
       const pathArr = path.split("/").slice(1);
       let pathArrLen = pathArr.length;
-      createTreeNodes(pathArr, pathArrLen, treeNodes);
+      createTreeNodes(pathArr, pathArrLen, route, treeNodes);
     }
     return treeNodes;
   }, [createTreeNodes, props.pagesInfo]);
 
-  const onPageClicked = useCallback((urlPath: string) => {
-    canvasApi.navigatePage(urlPath);
-  }, []);
+  const onSelect: DirectoryTreeProps["onSelect"] = (keys, info) => {
+    console.log("Trigger Select", keys, info);
+  };
+
+  const onExpand: DirectoryTreeProps["onExpand"] = (keys, info) => {
+    const node =
+      info.node.children!.length > 0
+        ? (info.node.children![0].key as string)
+        : "";
+    if (node !== "") canvasApi.navigatePage(node);
+  };
+
+  // const onPageClicked = useCallback((urlPath: string) => {
+  //   canvasApi.navigatePage(urlPath);
+  // }, []);
 
   console.log("Page", treeNodes);
 
   return (
     <div>
       <div onClick={props.onCloseClicked}>Close Page Tree</div>
-      {props.pagesInfo.map((pageInfo) => {
+      {/* {props.pagesInfo.map((pageInfo) => {
         console.log("pageInfo", pageInfo);
         return (
           <div
@@ -87,7 +102,12 @@ export const PageTree: React.FC<{
             {pageInfo.routeObjectPath}
           </div>
         );
-      })}
+      })} */}
+      <DirectoryTree
+        onSelect={onSelect}
+        onExpand={onExpand}
+        treeData={treeNodes}
+      />
     </div>
   );
 };
