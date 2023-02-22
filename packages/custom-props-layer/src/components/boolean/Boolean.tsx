f0bc3dbaf7f475f4c208c5ff6642526e39b3f0c3
Jonathan Alvares <alvaresjonathan599@gmail.com>
2023-02-22 21:42:39 +0530

    Documented boolean

--- packages/custom-props-layer/src/components/boolean/Boolean.tsx
+++ packages/custom-props-layer/src/components/boolean/Boolean.tsx
@@ -5,6 +5,14 @@ import { Checkbox } from "../commons/Checkbox";
 import { Label } from "../commons/Label";
 import { PropertyContainer } from "../commons/PropertyContainer";
 
+/*
+  Boolean custom prop is used in cases where a component needs a boolean variable.
+  Example: The alert component showing the close button.
+           If the user wants the close button he has to select it.
+  Declaration and Initialisation is stored in Component.manifest.ts
+  If the Component is an Alert component then Alert.manifest.ts
+*/
+
 export const Boolean: React.FC<ComponentProps> = (props) => {
   const selector = useMemo(() => {
     return props.selector || [];
