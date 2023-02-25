34467fff009c578b608243fa6abbd32cb1792494
cruxcode <swaroopshyam0@gmail.com>
2023-02-24 19:54:49 -0800

    overlay for hover done

--- packages/atri-app-core/src/editor-components/CanvasOverlay/CanvasOverlay.tsx
+++ packages/atri-app-core/src/editor-components/CanvasOverlay/CanvasOverlay.tsx
@@ -1,8 +1,10 @@
 import { useDragDrop, useSelectHints } from "./hooks";
+import { useHoverHints } from "./hooks/useHoverEvents";
 
 export function CanvasOverlay() {
   const { dragFC, dragOverlayStyle } = useDragDrop();
   useSelectHints();
+  useHoverHints();
   return (
     <>
       {dragFC?.Comp && dragOverlayStyle ? (
