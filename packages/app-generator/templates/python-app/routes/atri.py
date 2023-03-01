56e596e64497206fe61b158878e1c7dc2111b20b
cruxcode <swaroopshyam0@gmail.com>
2023-03-01 14:07:09 -0800

    removed app-scripts & app-generator package

--- packages/app-generator/templates/python-app/routes/atri.py
+++
@@ -1,5 +0,0 @@
-from typing import TypedDict
-
-class Atri(TypedDict):
-    _route: str
-    _query: str
--- packages/app-scripts/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./shared/common.webpack.config";
-export * from "./shared/server.webpack.config";
-export * from "./shared/webpack.config";
-export * from "./shared/utils";
