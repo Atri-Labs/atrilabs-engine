04d72cf307b48428b7de77a30a58b621e3da910d
cruxcode <swaroopshyam0@gmail.com>
2023-03-11 20:39:43 -0800

    fix: added missing deps to build scripts

--- packages/commands/src/scripts/create-atri-app.ts
+++ packages/commands/src/scripts/create-atri-app.ts
@@ -57,7 +57,7 @@ function createPackageJSON(
       scripts: {
         dev: "dev-atri-app -d '#@atrilabs/react-component-manifests' -a '@atrilabs/utils' -i '@atrilabs/utils'",
         editor: "APP_HOSTNAME='http://localhost:3000' dev-atri-editor",
-        "gen-py-app": "gen-py-app",
+        "gen-py-app": "gen-py-app -i '@atrilabs/utils' -a '@atrilabs/utils'",
         "gen-py-classes": "gen-py-classes",
         "dev-py-app": "dev-py-app",
       },
