e93cec537a2a02ad0f80a08c001d7b7716f6055b
cruxcode <swaroopshyam0@gmail.com>
2023-03-14 11:05:42 -0700

    added plan for building atri app

---
+++ packages/commands/src/scripts/build-atri-app/build-atri-app.ts
@@ -0,0 +1,20 @@
+/**
+ *
+ * 1. Create a temporary folder for the build inside dist/.
+ *
+ * 2. From events.json file to create nodes.
+ *
+ * 3. Convert component nodes into custom.json, callbacks.json, styles.css for each page.
+ *
+ * 4. Import all the 4 files in the page.
+ *
+ * 5. Write some code for rendering from custom.json & callbacks.json.
+ * The code should be similar to live preview code.
+ *
+ * 6. Generate two JS files for each page - index.client.js & index.server.js.
+ *
+ * 7. Generate py-classes from manifests.
+ *
+ * 8. Generate py-models from nodes.
+ *
+ */
