52e2680deb9f5a4e6c7dcf46696e5998dcf7079b
cruxcode <swaroopshyam0@gmail.com>
2023-03-22 00:16:49 -0700

    css string from components props

--- packages/commands/src/scripts/build-atri-app/build-atri-app.ts
+++ packages/commands/src/scripts/build-atri-app/build-atri-app.ts
@@ -2,16 +2,17 @@
 import { extractParams } from "@atrilabs/commands-builder";
 import path from "path";
 import { buildServerSide } from "./buildServerSide";
+import { getPagesInfo } from "./utils";
 
 /**
  *
- * 1. Create a temporary folder for the build inside dist/.
+ * 1. [O] Create a temporary folder for the build inside dist/.
  *
- * 2. From events.json file to create nodes.
+ * 2. [O] From events.json file to create nodes.
  *
- * 3. Convert component nodes into custom.json, callbacks.json, styles.css for each page.
+ * 3. [X] Convert component nodes into custom.json, callbacks.json, styles.css for each page.
  *
- * 4. Import all the 4 files in the page.
+ * 4. [X] Import all the 4 files in the page.
  *
  * 5. Write some code for rendering from custom.json & callbacks.json.
  * The code should be similar to live preview code.
@@ -28,7 +29,8 @@ async function main() {
   const params = extractParams();
   const outputDir = path.resolve("dist", "app-build");
   params.paths.outputDir = outputDir;
-  await buildServerSide(JSON.parse(JSON.stringify(params)));
+  const pagesInfo = await getPagesInfo();
+  await buildServerSide({ ...JSON.parse(JSON.stringify(params)), pagesInfo });
 }
 
 main().catch(console.log);
--- packages/commands/src/scripts/build-atri-app/utils.ts
+++ packages/commands/src/scripts/build-atri-app/utils.ts
@@ -4,12 +4,73 @@ import {
   readDirStructure,
 } from "@atrilabs/atri-app-core";
 import path from "path";
+import { PageInfo } from "./types";
+import fs from "fs";
+import { AnyEvent } from "@atrilabs/forest";
+import {
+  createForestFromEvents,
+  getComponentsFromNodes,
+  jssToCss,
+} from "../../commons/build-utils";
 
-export async function getAllPages() {
+export function getEventsFile(pagePath: string) {
+  const eventsJSONFile = pagePath.replace(/(\.((js|ts)x?))$/, ".events.json");
+  if (fs.existsSync(eventsJSONFile)) {
+    return eventsJSONFile;
+  }
+  return undefined;
+}
+
+export function getComponentsFromEventsPath(eventsPath: string) {
+  try {
+    const events: AnyEvent[] = JSON.parse(
+      fs.readFileSync(eventsPath).toString()
+    );
+    const forest = createForestFromEvents(events);
+    const components = getComponentsFromNodes(forest);
+    return components;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export async function getPagesInfo(): Promise<PageInfo[]> {
   const pagePaths = await readDirStructure(path.resolve("pages"));
   const irs = dirStructureToIR(pagePaths);
   const routeObjectPaths = pathsIRToRouteObjectPaths(irs);
   return pagePaths.map((pagePath, index) => {
-    return { pagePath, routeObjectPath: routeObjectPaths[index] };
+    const eventsPath = getEventsFile(pagePath);
+    const components = eventsPath
+      ? getComponentsFromEventsPath(eventsPath)
+      : [];
+    return {
+      pagePath,
+      routeObjectPath: routeObjectPaths[index]!,
+      eventsPath: getEventsFile(pagePath),
+      components,
+    };
   });
+}
+
+export function createStoreFromComponents(
+  components: ReturnType<typeof getComponentsFromNodes>
+) {
+  const store: { [alias: string]: any } = {};
+  components.forEach((component) => {
+    const copyOfProps = JSON.parse(JSON.stringify(component.props));
+    delete copyOfProps["styles"];
+    store[component.alias] = copyOfProps;
+  });
+  return store;
+}
+
+export async function createCssText(
+  components: ReturnType<typeof getComponentsFromNodes>
+) {
+  const cssObj: { [alias: string]: React.CSSProperties } = {};
+  components.forEach((component) => {
+    if (component.props["styles"])
+      cssObj[component.alias] = component.props["styles"];
+  });
+  return await jssToCss(cssObj);
 }
